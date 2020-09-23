9c4cf9d037ea39235aae5a3e40ce7ebbac7b34bd
jakemulley <jakemulley@users.noreply.github.com>
2020-09-23 13:16:43 +0100

    Format the terraform file correctly

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,7 +1,7 @@
 data "aws_iam_policy_document" "organisation-management" {
   statement {
-    sid = "DenyAllApartFromOrganisationManagement"
-    effect = "Deny"
+    sid     = "DenyAllApartFromOrganisationManagement"
+    effect  = "Deny"
     actions = ["*"]
     not_actions = [
       # Note that this doesn't allow the account to delete Organisational Units
@@ -21,12 +21,12 @@ resource "aws_iam_user" "terraform-organisation-management" {
 }
 
 resource "aws_iam_policy" "terraform-organisation-management-policy" {
-  name = "TerraformOrganisationManagementPolicy"
+  name        = "TerraformOrganisationManagementPolicy"
   description = "A policy that allows Terraform to only manage organisations"
-  policy = data.aws_iam_policy_document.organisation-management.json
+  policy      = data.aws_iam_policy_document.organisation-management.json
 }
 
 resource "aws_iam_user_policy_attachment" "terraform-organisation-management-policy-attachment" {
-  user = aws_iam_user.terraform-organisation-management.name
+  user       = aws_iam_user.terraform-organisation-management.name
   policy_arn = aws_iam_policy.terraform-organisation-management-policy.arn
 }
