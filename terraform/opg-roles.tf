b0d4ef88fceb7d448a684c4480e756911c47f87d
Tom Withers <thomas.withers@digital.justice.gov.uk>
2020-09-30 14:04:37 +0100

    Initial role for OPG engineers in the root aws account

---
+++ terraform/opg-roles.tf
@@ -0,0 +1,31 @@
+module "opg-sso-operator" {
+  source             = "./modules/opg_role"
+  name               = "opg-operator"
+  user_arns          = local.opg_engineers
+  custom_policy_json = data.aws_iam_policy_document.opg_operator.json
+}
+
+data "aws_iam_policy_document" "opg_operator" {
+  statement {
+    sid       = "AllowOrganizationsAccess"
+    effect    = "Allow"
+    actions   = ["organizations:*"]
+    resources = ["*"]
+  }
+  statement {
+    sid       = "AllowSSOAccess"
+    effect    = "Allow"
+    actions   = ["sso:*"]
+    resources = ["*"]
+  }
+}
+
+
+locals {
+  # I've decided to hard code our ARNs, I don't want to make this repo depend on our accounts.
+  # Once SSO has been implemented we can get rid of these anyway
+  opg_engineers = [
+    "arn:aws:iam::631181914621:user/thomas.withers",
+    "arn:aws:iam::631181914621:user/andrew.pearce",
+  ]
+}
