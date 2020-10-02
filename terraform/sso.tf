3842299bcd93632b7630c97b60d07f6896d01cd1
jakemulley <jakemulley@users.noreply.github.com>
2020-10-02 19:27:07 +0100

    Configure AWS SSO

---
+++ terraform/sso.tf
@@ -0,0 +1,12 @@
+module "sso" {
+  source                     = "https://github.com/ministryofjustice/moj-terraform-aws-sso"
+  auth0_tenant_domain        = "ministryofjustice.eu.auth0.com"
+  auth0_client_id            = local.auth0_saml.client_id
+  auth0_client_secret        = local.auth0_saml.client_secret
+  auth0_github_client_id     = local.github_saml.client_id
+  auth0_github_client_secret = local.github_saml.client_secret
+  auth0_aws_sso_acs_url      = local.aws_saml.acs_url
+  auth0_aws_sso_issuer_url   = local.aws_saml.issuer_url
+  auth0_github_allowed_orgs  = ["ministryofjustice"]
+  auth0_allowed_domains      = ["justice.gov.uk", "digital.justice.gov.uk"]
+}
