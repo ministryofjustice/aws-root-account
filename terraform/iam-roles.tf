4d516b66fcf335729ce2a518516a47b271187b79
jakemulley <jakemulley@users.noreply.github.com>
2020-11-25 16:06:04 +0000

    Bring IAM roles into Terraform management

---
+++ terraform/iam-roles.tf
@@ -0,0 +1,87 @@
+# Service-linked roles
+resource "aws_iam_service_linked_role" "organizations" {
+  aws_service_name = "organizations.amazonaws.com"
+  description      = "Service-linked role used by AWS Organizations to enable integration of other AWS services with Organizations."
+}
+
+resource "aws_iam_service_linked_role" "securityhub" {
+  aws_service_name = "securityhub.amazonaws.com"
+  description      = "A service-linked role required for AWS Security Hub to access your resources."
+}
+
+resource "aws_iam_service_linked_role" "sso" {
+  aws_service_name = "sso.amazonaws.com"
+  description      = "Service-linked role used by AWS SSO to manage AWS resources, including IAM roles, policies and SAML IdP on your behalf."
+}
+
+resource "aws_iam_service_linked_role" "support" {
+  aws_service_name = "support.amazonaws.com"
+  description      = "Enables resource access for AWS to provide billing, administrative and support services"
+}
+
+resource "aws_iam_service_linked_role" "trustedadvisor" {
+  aws_service_name = "trustedadvisor.amazonaws.com"
+  description      = "Access for the AWS Trusted Advisor Service to help reduce cost, increase performance, and improve security of your AWS environment."
+}
+
+# Other roles
+## IAM ReadOnly Access Role
+resource "aws_iam_role" "iam-read-only-access-role" {
+  name = "IAMReadOnlyAccessRole"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowAssumeIAMReadOnlyAccessRole",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::${local.caller_identity.account_id}:root"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+## lambda_basic_execution-test
+resource "aws_iam_role" "lambda_basic_execution-test" {
+  name = "lambda_basic_execution-test"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+## lambda-iam-generate-report-role
+resource "aws_iam_role" "lambda-iam-generate-report-role" {
+  name = "lambda-iam-generate-report-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
