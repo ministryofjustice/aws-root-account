0869d1158cec7e5f85ef5231be2a16b743280166
jakemulley <jakemulley@users.noreply.github.com>
2020-11-26 14:43:54 +0000

    Bring AWS Organizations CICA OU accounts into Terraform management

---
+++ terraform/organizations-accounts-cica.tf
@@ -0,0 +1,17 @@
+# CICA OU
+resource "aws_organizations_account" "cica" {
+  name      = "CICA"
+  email     = local.account_emails["CICA"][0]
+  parent_id = aws_organizations_organizational_unit.cica.id
+
+  lifecycle {
+    # If any of these attributes are changed, it attempts to destroy and recreate the account,
+    # so we should ignore the changes to prevent this from happening.
+    ignore_changes = [
+      name,
+      email,
+      iam_user_access_to_billing,
+      role_name
+    ]
+  }
+}
