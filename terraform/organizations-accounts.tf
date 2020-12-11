0426f197f7c36fcadc8ef4c1394aa2af665bc24f
jakemulley <jakemulley@users.noreply.github.com>
2020-12-11 17:19:41 +0000

    Group the Modernisation Platform account in the correct OU

--- terraform/organizations-accounts.tf
+++ terraform/organizations-accounts.tf
@@ -121,28 +121,6 @@ resource "aws_organizations_policy_attachment" "electronic-monitoring-infrastruc
   target_id = aws_organizations_account.electronic-monitoring-infrastructure-dev.id
 }
 
-resource "aws_organizations_account" "modernisation-platform" {
-  name      = "Modernisation Platform"
-  email     = local.account_emails["Modernisation Platform"][0]
-  parent_id = aws_organizations_organization.default.roots[0].id
-
-  lifecycle {
-    # If any of these attributes are changed, it attempts to destroy and recreate the account,
-    # so we should ignore the changes to prevent this from happening.
-    ignore_changes = [
-      name,
-      email,
-      iam_user_access_to_billing,
-      role_name
-    ]
-  }
-}
-
-resource "aws_organizations_policy_attachment" "modernisation-platform" {
-  policy_id = "p-FullAWSAccess"
-  target_id = aws_organizations_account.modernisation-platform.id
-}
-
 resource "aws_organizations_account" "moj-billing-management" {
   name      = "MoJ Billing Management"
   email     = local.account_emails["MoJ Billing Management"][0]
