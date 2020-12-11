9cbd04bf8425a83d8419816f56b94b689fee1403
jakemulley <jakemulley@users.noreply.github.com>
2020-12-11 17:11:23 +0000

    Group MoJ Official accounts in the same OU

--- terraform/organizations-accounts-workplace-technology.tf
+++ terraform/organizations-accounts-workplace-technology.tf
@@ -1,3 +1,10 @@
+# AWS accounts for Workplace Technology and MoJ Official
+locals {
+  tags-workplace-technology = {
+    business-unit = "HQ"
+  }
+}
+
 # Workplace Technology OU
 resource "aws_organizations_account" "workplace-tech-proof-of-concept-development" {
   name      = "Workplace Tech Proof Of Concept Development"
@@ -14,6 +21,8 @@ resource "aws_organizations_account" "workplace-tech-proof-of-concept-developmen
       role_name
     ]
   }
+
+  tags = local.tags-workplace-technology
 }
 
 resource "aws_organizations_policy_attachment" "workplace-tech-proof-of-concept-development" {
@@ -36,6 +45,8 @@ resource "aws_organizations_account" "wptpoc" {
       role_name
     ]
   }
+
+  tags = local.tags-workplace-technology
 }
 
 resource "aws_organizations_policy_attachment" "wptpoc" {
@@ -58,6 +69,8 @@ resource "aws_organizations_account" "moj-official-production" {
       role_name
     ]
   }
+
+  tags = local.tags-workplace-technology
 }
 
 resource "aws_organizations_policy_attachment" "moj-official-production" {
@@ -80,6 +93,8 @@ resource "aws_organizations_account" "moj-official-pre-production" {
       role_name
     ]
   }
+
+  tags = local.tags-workplace-technology
 }
 
 resource "aws_organizations_policy_attachment" "moj-official-pre-production" {
@@ -102,9 +117,83 @@ resource "aws_organizations_account" "moj-official-development" {
       role_name
     ]
   }
+
+  tags = local.tags-workplace-technology
 }
 
 resource "aws_organizations_policy_attachment" "moj-official-development" {
   policy_id = "p-FullAWSAccess"
   target_id = aws_organizations_account.moj-official-development.id
+}
+
+resource "aws_organizations_account" "moj-official-public-key-infrastructure-dev" {
+  name      = "MOJ Official (Public Key Infrastructure Dev)"
+  email     = local.account_emails["MOJ Official (Public Key Infrastructure Dev)"][0]
+  parent_id = aws_organizations_organizational_unit.workplace-technology.id
+
+  lifecycle {
+    # If any of these attributes are changed, it attempts to destroy and recreate the account,
+    # so we should ignore the changes to prevent this from happening.
+    ignore_changes = [
+      name,
+      email,
+      iam_user_access_to_billing,
+      role_name
+    ]
+  }
+
+  tags = local.tags-workplace-technology
+}
+
+resource "aws_organizations_policy_attachment" "moj-official-public-key-infrastructure-dev" {
+  policy_id = "p-FullAWSAccess"
+  target_id = aws_organizations_account.moj-official-public-key-infrastructure-dev.id
+}
+
+resource "aws_organizations_account" "moj-official-public-key-infrastructure" {
+  name      = "MOJ Official (Public Key Infrastructure)"
+  email     = local.account_emails["MOJ Official (Public Key Infrastructure)"][0]
+  parent_id = aws_organizations_organizational_unit.workplace-technology.id
+
+  lifecycle {
+    # If any of these attributes are changed, it attempts to destroy and recreate the account,
+    # so we should ignore the changes to prevent this from happening.
+    ignore_changes = [
+      name,
+      email,
+      iam_user_access_to_billing,
+      role_name
+    ]
+  }
+
+  tags = local.tags-workplace-technology
+}
+
+resource "aws_organizations_policy_attachment" "moj-official-public-key-infrastructure" {
+  policy_id = "p-FullAWSAccess"
+  target_id = aws_organizations_account.moj-official-public-key-infrastructure.id
+}
+
+resource "aws_organizations_account" "moj-official-shared-services" {
+  name      = "MOJ Official (Shared Services)"
+  email     = local.account_emails["MOJ Official (Shared Services)"][0]
+  parent_id = aws_organizations_organizational_unit.workplace-technology.id
+
+  lifecycle {
+    # If any of these attributes are changed, it attempts to destroy and recreate the account,
+    # so we should ignore the changes to prevent this from happening.
+    ignore_changes = [
+      name,
+      email,
+      iam_user_access_to_billing,
+      role_name
+    ]
+  }
+
+  tags = local.tags-workplace-technology
+}
+
+resource "aws_organizations_policy_attachment" "moj-official-shared-services" {
+  policy_id = "p-FullAWSAccess"
+  target_id = aws_organizations_account.moj-official-shared-services.id
 }
