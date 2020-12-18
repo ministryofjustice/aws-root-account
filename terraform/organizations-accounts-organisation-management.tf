b0c82e338e1f0decbd7edf7845c80809ed02174c
Jake Mulley <jakemulley@users.noreply.github.com>
2020-12-18 14:39:42 +0000

    Revert "Remove policy that is automatically enabled by AWS"

--- terraform/organizations-accounts-organisation-management.tf
+++ terraform/organizations-accounts-organisation-management.tf
@@ -29,6 +29,11 @@ resource "aws_organizations_account" "organisation-logging" {
   )
 }
 
+resource "aws_organizations_policy_attachment" "organisation-logging" {
+  policy_id = "p-FullAWSAccess"
+  target_id = aws_organizations_account.organisation-logging.id
+}
+
 # Organisation security account
 resource "aws_organizations_account" "organisation-security" {
   name      = "organisation-security"
@@ -51,4 +56,9 @@ resource "aws_organizations_account" "organisation-security" {
       component = "Security"
     }
   )
+}
+
+resource "aws_organizations_policy_attachment" "organisation-security" {
+  policy_id = "p-FullAWSAccess"
+  target_id = aws_organizations_account.organisation-security.id
 }
