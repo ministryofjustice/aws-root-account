75b6f27a5dce03cd2ee585a266b9e74f22395286
jakemulley <jakemulley@users.noreply.github.com>
2020-12-31 18:27:06 +0000

    Enable AWS Config multi-region, multi-account aggregation

--- terraform/config-aggregation.tf
+++ terraform/config-aggregation.tf
@@ -56,3 +56,72 @@ module "config-aggregation-sns-eu-west-2" {
     }
   )
 }
+
+# Configure an SNS topic in organisation-security for aggregated Config alerts for eu-west-1
+# Note this needs to be configured for each region, as AWS Config doesn't support
+# cross-region delivery to an SNS topic.
+module "config-aggregation-sns-eu-west-1" {
+  source = "./modules/config-aggregation-sns"
+  providers = {
+    aws = aws.organisation-security-eu-west-1
+  }
+
+  # Specify account IDs that can send notifications to the topic
+  enrolled_account_ids = [
+    for account in local.enrolled_into_config :
+    account.id
+  ]
+
+  # Tags to apply, where applicable
+  tags = merge(
+    local.tags-organisation-management, {
+      component = "Security"
+    }
+  )
+}
+
+# Multi-region, Multi-account aggregation (MRMAA)
+## Create an IAM role with appropriate permissions for AWS Config in organisation-security (delegated administrator)
+module "config-organisation-security-iam-role" {
+  source = "./modules/config-iam-role"
+  providers = {
+    aws = aws.organisation-security-eu-west-2
+  }
+
+  s3_bucket_arn = module.config-aggregation-bucket.s3_bucket_arn
+  sns_topic_arns = [
+    module.config-aggregation-sns-eu-west-2.sns_topic_arn,
+    module.config-aggregation-sns-eu-west-1.sns_topic_arn
+  ]
+}
+
+## Enable AWS Config in eu-west-2 within the organisation-security account
+## You need to enable Config in the delegated administrator account before you can create an aggregator.
+module "config-organisation-security-eu-west-2" {
+  source = "./modules/config"
+  providers = {
+    aws = aws.organisation-security-eu-west-2
+  }
+
+  s3_bucket_name = module.config-aggregation-bucket.s3_bucket_name
+  sns_topic_arn  = module.config-aggregation-sns-eu-west-2.sns_topic_arn
+  iam_role_arn   = module.config-organisation-security-iam-role.role_arn
+  home_region    = "eu-west-2"
+}
+
+resource "aws_config_configuration_aggregator" "aggregator" {
+  provider = aws.organisation-security-eu-west-2
+
+  name = "aggregator"
+
+  # Once all accounts are enrolled, you can use: `organization_aggregation_source {}` to enable the aggregator
+  # for all member accounts without having to specify them individually or work with singular Config Authorizations
+  # on a per-account, per-region basis.
+  account_aggregation_source {
+    account_ids = [
+      for account in local.enrolled_into_config :
+        account.id
+    ]
+    regions = ["eu-west-2", "eu-west-1"]
+  }
+}
