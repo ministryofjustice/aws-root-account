450b35ec9e52a7cb482edaf4ead8760ab6c61a09
jakemulley <jakemulley@users.noreply.github.com>
2021-03-08 07:37:22 +0000

    Add an S3 bucket to store tag policy reports

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -84,6 +84,95 @@ resource "aws_s3_bucket_policy" "moj-cur-reports-bucket-policy" {
   policy = data.aws_iam_policy_document.moj-cur-reports-bucket-policy.json
 }
 
+# S3 bucket for storing tagging policy reports
+resource "aws_s3_bucket" "tagging-policy-reports" {
+  # The bucket for Tag Policy reports used MUST reside in us-east-1, see:
+  # https://docs.aws.amazon.com/ARG/latest/userguide/tag-policies-arg-evaluating-org-wide-compliance.html
+  provider = aws.aws-root-account-us-east-1
+
+  bucket_prefix = "tagging-policy-reports-"
+  acl           = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  tags = local.root_account
+
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "tagging-policy-reports" {
+  # The bucket for Tag Policy reports used MUST reside in us-east-1, see:
+  # https://docs.aws.amazon.com/ARG/latest/userguide/tag-policies-arg-evaluating-org-wide-compliance.html
+  provider = aws.aws-root-account-us-east-1
+
+  bucket = aws_s3_bucket.tagging-policy-reports.id
+
+  # Block public ACLs
+  block_public_acls = true
+
+  # Block public bucket policies
+  block_public_policy = true
+
+  # Ignore public ACLs
+  ignore_public_acls = true
+
+  # Restrict public bucket policies
+  restrict_public_buckets = true
+}
+
+# S3 bucket policy for Tag policies. See:
+# https://docs.aws.amazon.com/ARG/latest/userguide/tag-policies-prereqs.html
+data "aws_iam_policy_document" "tagging-policy" {
+  version = "2012-10-17"
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetBucketAcl"
+    ]
+    resources = [aws_s3_bucket.tagging-policy-reports.arn]
+
+    principals {
+      type        = "Service"
+      identifiers = ["tagpolicies.tag.amazonaws.com"]
+    }
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = ["${aws_s3_bucket.tagging-policy-reports.arn}/AwsTagPolicies/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["tagpolicies.tag.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "tagging-policy" {
+  # The bucket for Tag Policy reports used MUST reside in us-east-1, see:
+  # https://docs.aws.amazon.com/ARG/latest/userguide/tag-policies-arg-evaluating-org-wide-compliance.html
+  provider = aws.aws-root-account-us-east-1
+
+  bucket = aws_s3_bucket.tagging-policy-reports.bucket
+  policy = data.aws_iam_policy_document.tagging-policy.json
+
+  depends_on = [aws_s3_bucket_public_access_block.tagging-policy-reports]
+}
+
 # S3 buckets in Ireland
 ## S3 bucket for moj-iam-credential-reports
 resource "aws_s3_bucket" "moj-iam-credential-reports" {
