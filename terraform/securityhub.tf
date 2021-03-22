580c6e4bd60eda68dc695499a70bae4d7f6cbd7e
jakemulley <jakemulley@users.noreply.github.com>
2021-03-22 18:20:22 +0000

    Enrol VCMS and Delius Core non-production accounts into Security Hub

--- terraform/securityhub.tf
+++ terraform/securityhub.tf
@@ -13,11 +13,13 @@
 locals {
   enrolled_into_securityhub = concat([
     { id = local.caller_identity.account_id, name = "MoJ root account" },
+    aws_organizations_account.delius-core-non-prod,
     aws_organizations_account.laa-development,
     aws_organizations_account.laa-staging,
     aws_organizations_account.laa-test,
     aws_organizations_account.legal-aid-agency,
     aws_organizations_account.modernisation-platform,
+    aws_organizations_account.vcms-non-prod,
   ], local.modernisation-platform-managed-account-ids)
 }
 
