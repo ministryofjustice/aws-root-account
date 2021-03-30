74e7e3b74bbe88e4b4fcdd854975196c0313a997
jakemulley <jakemulley@users.noreply.github.com>
2021-03-30 09:46:16 +0100

    Update CloudTrail tags to include source-code

--- terraform/cloudtrail.tf
+++ terraform/cloudtrail.tf
@@ -4,5 +4,7 @@ module "cloudtrail" {
     aws.replication-region = aws.aws-root-account-eu-west-1
   }
   replication_role_arn = module.iam_s3_replication_role.role.arn
-  tags                 = local.root_account
+  tags = merge(local.root_account, {
+    source-code = "${local.github_repository}/cloudtrail.tf"
+  })
 }
