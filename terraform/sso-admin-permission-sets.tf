7c8fb0a057548cafae1a1f864abb4dc09e38ccb6
David Elliott <david.elliott@digital.justice.gov.uk>
2021-10-01 14:37:50 +0100

    Give MP developer role kms decrypt
    Turns out the ReadOnlyAccess role doesn't have kms decrypt permissions.
    Adding here to allow developers to decrypt KMS encrypted items (if the
    KMS policy allows them to)

--- terraform/sso-admin-permission-sets.tf
+++ terraform/sso-admin-permission-sets.tf
@@ -189,7 +189,8 @@ data "aws_iam_policy_document" "modernisation-platform-developer-additional" {
       "ssm:TerminateSession",
       "ssm:ResumeSession",
       "ssm:DescribeSessions",
-      "ssm:GetConnectionStatus"
+      "ssm:GetConnectionStatus",
+      "kms:Decrypt*"
     ]
 
     resources = ["*"]
