956f81ba24d7e9cb7a6a40278ed956db4d5d0022
jakemulley <jakemulley@users.noreply.github.com>
2021-10-02 01:53:21 +0100

    Update iam_s3_replication_role to v3.0.0

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -9,7 +9,7 @@ module "iam_password_policy" {
 
 # IAM role for S3 bucket replication
 module "iam_s3_replication_role" {
-  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket-replication-role?ref=v1.0.0"
+  source = "github.com/ministryofjustice/modernisation-platform-terraform-s3-bucket-replication-role?ref=v3.0.0"
   buckets = [
     module.cloudtrail.s3_bucket.arn,
     module.cloudtrail.log_bucket.arn
