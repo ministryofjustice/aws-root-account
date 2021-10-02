87db87ffd0596d61c35feba20b9143fe541ca79e
jakemulley <jakemulley@users.noreply.github.com>
2021-10-02 01:21:23 +0100

    GuardDuty: Use configuration_aliases rather than provider blocks for module providers

--- terraform/modules/guardduty/versions.tf
+++ terraform/modules/guardduty/versions.tf
@@ -3,7 +3,11 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 3.27.0"
+      configuration_aliases = [
+        aws.root-account,
+        aws.delegated-administrator
+      ]
     }
   }
-  required_version = ">= 0.14.2"
+  required_version = ">= 0.15.0"
 }
