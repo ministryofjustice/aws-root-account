bc4fb1d61591a3632a4221937087b88546c6dfeb
jakemulley <jakemulley@users.noreply.github.com>
2021-12-02 23:45:01 +0000

    Change references to opg-refund-develop, moj-opg-sirius-development, moj-opg-sirius-production to appropriate opg-modernising-lpa environments

--- terraform/organizations-accounts-opg-lpa-refunds.tf
+++ terraform/organizations-accounts-opg-lpa-refunds.tf
@@ -1,24 +1,4 @@
 # OPG OU: LPA Refunds
-resource "aws_organizations_account" "opg-refund-develop" {
-  name      = "opg-refund-develop"
-  email     = local.aws_account_email_addresses["opg-refund-develop"][0]
-  parent_id = aws_organizations_organizational_unit.opg-lpa-refunds.id
-  tags = merge(local.tags-opg, {
-    application = "Lasting Power of Attorney Refunds/LPA Refunds"
-  })
-
-  lifecycle {
-    # If any of these attributes are changed, it attempts to destroy and recreate the account,
-    # so we should ignore the changes to prevent this from happening.
-    ignore_changes = [
-      name,
-      email,
-      iam_user_access_to_billing,
-      role_name
-    ]
-  }
-}
-
 resource "aws_organizations_account" "opg-refund-production" {
   name      = "opg-refund-production"
   email     = local.aws_account_email_addresses["opg-refund-production"][0]
--- terraform/organizations-accounts-opg-sirius.tf
+++ terraform/organizations-accounts-opg-sirius.tf
@@ -1,45 +1,4 @@
 # OPG OU: Sirius
-resource "aws_organizations_account" "moj-opg-sirius-production" {
-  name      = "MoJ OPG Sirius Production"
-  email     = local.aws_account_email_addresses["MoJ OPG Sirius Production"][0]
-  parent_id = aws_organizations_organizational_unit.opg-sirius.id
-  tags = merge(local.tags-opg, {
-    application   = "Sirius",
-    is-production = true
-  })
-
-  lifecycle {
-    # If any of these attributes are changed, it attempts to destroy and recreate the account,
-    # so we should ignore the changes to prevent this from happening.
-    ignore_changes = [
-      name,
-      email,
-      iam_user_access_to_billing,
-      role_name
-    ]
-  }
-}
-
-resource "aws_organizations_account" "moj-opg-sirius-development" {
-  name      = "MoJ OPG Sirius Development"
-  email     = local.aws_account_email_addresses["MoJ OPG Sirius Development"][0]
-  parent_id = aws_organizations_organizational_unit.opg-sirius.id
-  tags = merge(local.tags-opg, {
-    application = "Sirius"
-  })
-
-  lifecycle {
-    # If any of these attributes are changed, it attempts to destroy and recreate the account,
-    # so we should ignore the changes to prevent this from happening.
-    ignore_changes = [
-      name,
-      email,
-      iam_user_access_to_billing,
-      role_name
-    ]
-  }
-}
-
 resource "aws_organizations_account" "opg-sirius-dev" {
   name      = "opg-sirius-dev"
   email     = local.aws_account_email_addresses["opg-sirius-dev"][0]
