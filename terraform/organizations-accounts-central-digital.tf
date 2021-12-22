6d38a33a74ba1a0302124dc45d2c473454929e60
jakemulley <jakemulley@users.noreply.github.com>
2021-12-22 10:09:59 +0000

    Move MOJ CLA to Closed accounts

--- terraform/organizations-accounts-central-digital.tf
+++ terraform/organizations-accounts-central-digital.tf
@@ -33,23 +33,6 @@ resource "aws_organizations_account" "network-architecture" {
   }
 }
 
-resource "aws_organizations_account" "moj-cla" {
-  name      = "MoJ CLA"
-  email     = local.aws_account_email_addresses["MoJ CLA"][0]
-  parent_id = aws_organizations_organizational_unit.central-digital.id
-
-  lifecycle {
-    # If any of these attributes are changed, it attempts to destroy and recreate the account,
-    # so we should ignore the changes to prevent this from happening.
-    ignore_changes = [
-      name,
-      email,
-      iam_user_access_to_billing,
-      role_name
-    ]
-  }
-}
-
 resource "aws_organizations_account" "patterns" {
   name      = "Patterns"
   email     = local.aws_account_email_addresses["Patterns"][0]
