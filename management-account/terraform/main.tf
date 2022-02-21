1e434efaa271a2fea54aa9e9effbb8ea1d904fcc
jakemulley <jakemulley@users.noreply.github.com>
2022-02-21 16:06:27 +0000

    Configure Terraform S3 backend for management-account

---
+++ management-account/terraform/main.tf
@@ -0,0 +1,9 @@
+terraform {
+  # `backend` blocks do not support variables, so the bucket name is hard-coded here
+  backend "s3" {
+    bucket  = "moj-aws-root-account-terraform-state"
+    region  = "eu-west-2"
+    key     = "management-account/terraform.tfstate"
+    encrypt = true
+  }
+}
