ad92ef28b44b48e9e7f935b674165718b13b8c42
jakemulley <jakemulley@users.noreply.github.com>
2022-02-22 16:25:12 +0000

    Add HMPPS Victim Case Management System

---
+++ management-account/terraform/organizations-accounts-hmpps-vcms.tf
@@ -0,0 +1,143 @@
+locals {
+  tags_victim_case_management_system = local.tags_business_units.hmpps
+}
+
+resource "aws_organizations_account" "hmpps_victim_case_management_system_integration" {
+  name                       = "HMPPS Victim Case Management System Integration"
+  email                      = replace(local.aws_account_email_addresses_template, "{email}", "hmpps-vcms-integration")
+  iam_user_access_to_billing = "ALLOW"
+  parent_id                  = aws_organizations_organizational_unit.hmpps_vcms.id
+
+  tags = merge(local.tags_victim_case_management_system, {
+
+  })
+
+  lifecycle {
+    ignore_changes = [
+      email,
+      iam_user_access_to_billing,
+      name,
+      role_name,
+    ]
+  }
+}
+
+resource "aws_organizations_account" "hmpps_victim_case_management_system_performance" {
+  name                       = "HMPPS Victim Case Management System Performance"
+  email                      = replace(local.aws_account_email_addresses_template, "{email}", "hmpps-vcms-perf")
+  iam_user_access_to_billing = "ALLOW"
+  parent_id                  = aws_organizations_organizational_unit.hmpps_vcms.id
+
+  tags = merge(local.tags_victim_case_management_system, {
+
+  })
+
+  lifecycle {
+    ignore_changes = [
+      email,
+      iam_user_access_to_billing,
+      name,
+      role_name,
+    ]
+  }
+}
+
+resource "aws_organizations_account" "hmpps_victim_case_management_system_pre_production" {
+  name                       = "HMPPS Victim Case Management System Pre Production"
+  email                      = replace(local.aws_account_email_addresses_template, "{email}", "hmpps-vcms-pre-prod")
+  iam_user_access_to_billing = "ALLOW"
+  parent_id                  = aws_organizations_organizational_unit.hmpps_vcms.id
+
+  tags = merge(local.tags_victim_case_management_system, {
+
+  })
+
+  lifecycle {
+    ignore_changes = [
+      email,
+      iam_user_access_to_billing,
+      name,
+      role_name,
+    ]
+  }
+}
+
+resource "aws_organizations_account" "hmpps_victim_case_management_system_production" {
+  name                       = "HMPPS Victim Case Management System Production"
+  email                      = replace(local.aws_account_email_addresses_template, "{email}", "hmpps-vcms-prod")
+  iam_user_access_to_billing = "ALLOW"
+  parent_id                  = aws_organizations_organizational_unit.hmpps_vcms.id
+
+  tags = merge(local.tags_victim_case_management_system, {
+
+  })
+
+  lifecycle {
+    ignore_changes = [
+      email,
+      iam_user_access_to_billing,
+      name,
+      role_name,
+    ]
+  }
+}
+
+resource "aws_organizations_account" "hmpps_victim_case_management_system_stage" {
+  name                       = "HMPPS Victim Case Management System Stage"
+  email                      = replace(local.aws_account_email_addresses_template, "{email}", "hmpps-vcms-stage")
+  iam_user_access_to_billing = "ALLOW"
+  parent_id                  = aws_organizations_organizational_unit.hmpps_vcms.id
+
+  tags = merge(local.tags_victim_case_management_system, {
+
+  })
+
+  lifecycle {
+    ignore_changes = [
+      email,
+      iam_user_access_to_billing,
+      name,
+      role_name,
+    ]
+  }
+}
+
+resource "aws_organizations_account" "hmpps_victim_case_management_system_test" {
+  name                       = "HMPPS Victim Case Management System Test"
+  email                      = replace(local.aws_account_email_addresses_template, "{email}", "hmpps-vcms-test")
+  iam_user_access_to_billing = "ALLOW"
+  parent_id                  = aws_organizations_organizational_unit.hmpps_vcms.id
+
+  tags = merge(local.tags_victim_case_management_system, {
+
+  })
+
+  lifecycle {
+    ignore_changes = [
+      email,
+      iam_user_access_to_billing,
+      name,
+      role_name,
+    ]
+  }
+}
+
+resource "aws_organizations_account" "vcms_non_prod" {
+  name                       = "VCMS non-prod"
+  email                      = replace(local.aws_account_email_addresses_template, "{email}", "vcms_non_prod")
+  iam_user_access_to_billing = "ALLOW"
+  parent_id                  = aws_organizations_organizational_unit.hmpps_vcms.id
+
+  tags = merge(local.tags_victim_case_management_system, {
+
+  })
+
+  lifecycle {
+    ignore_changes = [
+      email,
+      iam_user_access_to_billing,
+      name,
+      role_name,
+    ]
+  }
+}
