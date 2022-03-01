d7b325af5b38460b9d14f63e6bdc9716285854f3
jakemulley <jakemulley@users.noreply.github.com>
2022-03-01 19:30:50 +0000

    Remove aws_organizations_accounts without a dependency

--- terraform/organizations-accounts-platforms-and-architecture-modernisation-platform.tf
+++ terraform/organizations-accounts-platforms-and-architecture-modernisation-platform.tf
@@ -26,30 +26,3 @@ resource "aws_organizations_account" "modernisation-platform" {
 
   tags = local.tags-modernisation-platform
 }
-
-# Below is a data source to get all Modernisation Platform-managed AWS accounts in a key => value
-# format, where key is the account name and value is their ID; which is stored in AWS Secrets Manager
-# on their side. We then store it in a local with the required map format:
-# { id => "account_id", name => "account name" } for the GuardDuty implementation
-# in this repository
-data "aws_secretsmanager_secret" "modernisation-platform-environment-management" {
-  provider = aws.modernisation-platform
-  name     = "environment_management"
-}
-
-data "aws_secretsmanager_secret_version" "modernisation-platform-account-ids" {
-  provider  = aws.modernisation-platform
-  secret_id = data.aws_secretsmanager_secret.modernisation-platform-environment-management.id
-}
-
-locals {
-  modernisation-platform-managed-account-ids = [
-    for key, value in jsondecode(data.aws_secretsmanager_secret_version.modernisation-platform-account-ids.secret_string).account_ids : {
-      id   = value
-      name = key
-    }
-  ]
-
-  modernisation_platform_environment_management = jsondecode(data.aws_secretsmanager_secret_version.modernisation-platform-account-ids.secret_string)
-
-}
