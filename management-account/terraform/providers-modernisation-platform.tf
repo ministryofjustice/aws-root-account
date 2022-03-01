215f01673a51f45cd07fa92545a3fc0687cb1d9f
jakemulley <jakemulley@users.noreply.github.com>
2022-03-01 19:35:43 +0000

    Add Modernisation Platform provider

---
+++ management-account/terraform/providers-modernisation-platform.tf
@@ -0,0 +1,9 @@
+# Provider for the Modernisation Platform, to get account IDs managed by the Modernisation Platform
+provider "aws" {
+  alias  = "modernisation-platform"
+  region = "eu-west-2"
+
+  assume_role {
+    role_arn = "arn:aws:iam::${aws_organizations_account.modernisation_platform.id}:role/OrganizationAccountAccessRole"
+  }
+}
