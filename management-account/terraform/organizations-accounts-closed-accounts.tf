a8ce9f9eead77cdb129140c2744945016b1cf058
jakemulley <jakemulley@users.noreply.github.com>
2022-03-29 09:22:29 +0100

    Move HMPPS Check My Diary Development to Closed accounts

--- management-account/terraform/organizations-accounts-closed-accounts.tf
+++ management-account/terraform/organizations-accounts-closed-accounts.tf
@@ -68,3 +68,23 @@ resource "aws_organizations_account" "hmpps_check_my_diary_production" {
     ]
   }
 }
+
+resource "aws_organizations_account" "hmpps_check_my_diary_development" {
+  name                       = "HMPPS Check My Diary Development"
+  email                      = replace(local.aws_account_email_addresses_template, "{email}", "hmpps-cmd-dev")
+  iam_user_access_to_billing = "ALLOW"
+  parent_id                  = aws_organizations_organizational_unit.closed_accounts.id
+
+  tags = merge(local.tags_hmpps, {
+
+  })
+
+  lifecycle {
+    ignore_changes = [
+      email,
+      iam_user_access_to_billing,
+      name,
+      role_name,
+    ]
+  }
+}
