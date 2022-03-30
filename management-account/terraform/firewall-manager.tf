0329ec28db2fc3ede59faf23a9dc1075edb2fab2
jakemulley <jakemulley@users.noreply.github.com>
2022-03-30 16:01:21 +0100

    Add clarification on Firewall Manager provider

--- management-account/terraform/firewall-manager.tf
+++ management-account/terraform/firewall-manager.tf
@@ -2,6 +2,7 @@
 # Firewall Manager #
 ####################
 resource "aws_fms_admin_account" "default" {
+  # Firewall Manager is a global service so needs to use the us-east-1 provider
   provider   = aws.us-east-1
   account_id = aws_organizations_account.organisation_security.id
 }
