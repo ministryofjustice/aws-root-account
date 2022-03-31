c78fb4ab5bf49919963380f3b5db75d02375f077
jakemulley <jakemulley@users.noreply.github.com>
2022-03-31 22:52:34 +0100

    Move Cost and Usage reports to management-account

---
+++ management-account/terraform/cost-and-usage-reports.tf
@@ -0,0 +1,35 @@
+resource "aws_cur_report_definition" "no_integrations" {
+  provider = aws.us-east-1
+
+  report_name                = "MOJ-CUR"
+  time_unit                  = "DAILY"
+  format                     = "textORcsv"
+  compression                = "ZIP"
+  additional_schema_elements = ["RESOURCES"]
+  additional_artifacts       = []
+  refresh_closed_reports     = true
+  report_versioning          = "CREATE_NEW_REPORT"
+
+  # S3 configuration
+  s3_bucket = module.cur_reports_s3_bucket.bucket_name
+  s3_region = "eu-west-2"
+  s3_prefix = "CUR"
+}
+
+resource "aws_cur_report_definition" "quicksight_integration" {
+  provider = aws.us-east-1
+
+  report_name                = "MOJ-CUR-QUICKSIGHT-INTEGRATION"
+  time_unit                  = "DAILY"
+  format                     = "textORcsv"
+  compression                = "ZIP"
+  additional_schema_elements = ["RESOURCES"]
+  additional_artifacts       = ["REDSHIFT", "QUICKSIGHT"]
+  refresh_closed_reports     = true
+  report_versioning          = "CREATE_NEW_REPORT"
+
+  # S3 configuration
+  s3_bucket = module.cur_reports_quicksight_s3_bucket.bucket_name
+  s3_region = "eu-west-2"
+  s3_prefix = "CUR"
+}
