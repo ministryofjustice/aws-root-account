8de9b1a9f9723ace4aa1e24f1069b6364a57d47a
jakemulley <jakemulley@users.noreply.github.com>
2022-04-01 13:59:06 +0100

    Firewall Manager: two policies, one with auto-remediation and one without

--- organisation-security/terraform/firewall-manager.tf
+++ organisation-security/terraform/firewall-manager.tf
@@ -2,9 +2,11 @@
 # Firewall Manager in eu-west-2 #
 #################################
 
-# Shield Advanced policy
-resource "aws_fms_policy" "eu_west_2_modernisation_platform_shield_advanced" {
-  name                               = "modernisation-platform"
+# Shield Advanced policy (auto-remediate)
+resource "aws_fms_policy" "eu_west_2_shield_advanced_auto_remediate" {
+  provider = aws.eu-west-2
+
+  name                               = "shield_advanced_auto_remediate"
   delete_all_policy_resources        = true
   delete_unused_fm_managed_resources = false
   exclude_resource_tags              = false
@@ -16,10 +18,40 @@ resource "aws_fms_policy" "eu_west_2_modernisation_platform_shield_advanced" {
   ]
 
   include_map {
-    account = toset([
-      for name, id in local.accounts.modernisation_platform_shield_advanced :
-      id
-    ])
+    account = try(toset(local.shield_advanced_auto_remediate.accounts), null)
+    orgunit = try(toset(local.shield_advanced_auto_remediate.organizational_units), null)
+  }
+
+  resource_tags = {
+    is-production = "false"
+  }
+
+  security_service_policy_data {
+    type = "SHIELD_ADVANCED"
+    managed_service_data = jsonencode({
+      type = "SHIELD_ADVANCED"
+    })
+  }
+}
+
+# Shield Advanced policy (don't auto remediate)
+resource "aws_fms_policy" "eu_west_2_shield_advanced_no_auto_remediate" {
+  provider = aws.eu-west-2
+
+  name                               = "shield_advanced_no_auto_remediate"
+  delete_all_policy_resources        = true
+  delete_unused_fm_managed_resources = false
+  exclude_resource_tags              = false
+  remediation_enabled                = false
+  resource_type_list = [
+    "AWS::EC2::EIP",
+    "AWS::ElasticLoadBalancing::LoadBalancer",
+    "AWS::ElasticLoadBalancingV2::LoadBalancer",
+  ]
+
+  include_map {
+    account = try(toset(local.shield_advanced_no_auto_remediate.accounts), null)
+    orgunit = try(toset(local.shield_advanced_no_auto_remediate.organizational_units), null)
   }
 
   resource_tags = {
