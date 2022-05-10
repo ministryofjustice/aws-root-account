e1e612420199ec5ddbabfe6b64f43bf22048e984
jakemulley <jakemulley@users.noreply.github.com>
2022-05-10 11:01:05 +0100

    Move MOJ DSD to Platforms and Architecture OU

--- management-account/terraform/organizations-accounts-central-digital.tf
+++ management-account/terraform/organizations-accounts-central-digital.tf
@@ -2,22 +2,3 @@ locals {
   tags_central_digital = merge(local.tags_business_units.hq)
 }
 
-resource "aws_organizations_account" "moj_digital_services" {
-  name                       = "MoJ Digital Services"
-  email                      = replace(local.aws_account_email_addresses_template, "{email}", "")
-  iam_user_access_to_billing = "ALLOW"
-  parent_id                  = aws_organizations_organizational_unit.central_digital.id
-
-  tags = merge(local.tags_central_digital, {
-    is-production = true
-  })
-
-  lifecycle {
-    ignore_changes = [
-      email,
-      iam_user_access_to_billing,
-      name,
-      role_name,
-    ]
-  }
-}
