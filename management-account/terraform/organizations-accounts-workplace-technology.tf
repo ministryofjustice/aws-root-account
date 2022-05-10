f6b977b7ff74dd6137939651e63d3df8baa27862
Richard Baguley <20583399+bagg3rs@users.noreply.github.com>
2022-05-10 10:10:39 +0100

    Add WTP landing zone account to sso for techops

--- management-account/terraform/organizations-accounts-workplace-technology.tf
+++ management-account/terraform/organizations-accounts-workplace-technology.tf
@@ -181,3 +181,21 @@ resource "aws_organizations_account" "wptpoc" {
     ]
   }
 }
+
+resource "aws_organizations_account" "wtp_landing_zone" {
+  name                       = "WTP Landing Zone"
+  email                      = replace(local.aws_account_email_addresses_template, "{email}", "wtp_landing_zone")
+  iam_user_access_to_billing = "ALLOW"
+  parent_id                  = aws_organizations_organizational_unit.workplace_technology.id
+
+  tags = local.tags_workplace_technology
+
+  lifecycle {
+    ignore_changes = [
+      email,
+      iam_user_access_to_billing,
+      name,
+      role_name,
+    ]
+  }
+}
