1068604ab996c04ed94ac56a021e8a6cee17dee8
jakemulley <jakemulley@users.noreply.github.com>
2022-05-11 22:01:30 +0100

    Move accounts out of Tactical Products organizational unit

--- management-account/terraform/organizations-accounts-hmcts.tf
+++ management-account/terraform/organizations-accounts-hmcts.tf
@@ -42,3 +42,57 @@ resource "aws_organizations_account" "manchester_traffic_dev" {
     ]
   }
 }
+
+resource "aws_organizations_account" "ministry_of_justice_courtfinder_prod" {
+  name                       = "Ministry of Justice Courtfinder Prod"
+  email                      = replace(local.aws_account_email_addresses_template, "{email}", "courtfinder-prod")
+  iam_user_access_to_billing = "ALLOW"
+  parent_id                  = aws_organizations_organizational_unit.hmcts.id
+
+  tags = local.tags_hmcts
+
+  lifecycle {
+    ignore_changes = [
+      email,
+      iam_user_access_to_billing,
+      name,
+      role_name,
+    ]
+  }
+}
+
+resource "aws_organizations_account" "moj_info_services_dev" {
+  name                       = "MoJ Info Services Dev"
+  email                      = replace(local.aws_account_email_addresses_template, "{email}", "courtfinder")
+  iam_user_access_to_billing = "ALLOW"
+  parent_id                  = aws_organizations_organizational_unit.hmcts.id
+
+  tags = local.tags_hmcts
+
+  lifecycle {
+    ignore_changes = [
+      email,
+      iam_user_access_to_billing,
+      name,
+      role_name,
+    ]
+  }
+}
+
+resource "aws_organizations_account" "tp_hmcts" {
+  name                       = "TP-HMCTS"
+  email                      = replace(local.aws_account_email_addresses_template, "{email}", "TP-HMCTS")
+  iam_user_access_to_billing = "ALLOW"
+  parent_id                  = aws_organizations_organizational_unit.hmcts.id
+
+  tags = local.tags_hmcts
+
+  lifecycle {
+    ignore_changes = [
+      email,
+      iam_user_access_to_billing,
+      name,
+      role_name,
+    ]
+  }
+}
