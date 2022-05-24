6bc5fd1645ef146f8beb52eda49c1f62de53c9ff
jakemulley <jakemulley@users.noreply.github.com>
2022-05-24 11:08:00 +0100

    Move Security Engineering to closed accounts

--- management-account/terraform/organizations-accounts-closed-accounts.tf
+++ management-account/terraform/organizations-accounts-closed-accounts.tf
@@ -206,3 +206,21 @@ resource "aws_organizations_account" "strategic_partner_gateway_non_production" 
     ]
   }
 }
+
+resource "aws_organizations_account" "security_engineering" {
+  name                       = "Security Engineering"
+  email                      = replace(local.aws_account_email_addresses_template, "{email}", "security_engineering")
+  iam_user_access_to_billing = "ALLOW"
+  parent_id                  = aws_organizations_organizational_unit.closed_accounts.id
+
+  tags = local.tags_security
+
+  lifecycle {
+    ignore_changes = [
+      email,
+      iam_user_access_to_billing,
+      name,
+      role_name,
+    ]
+  }
+}
