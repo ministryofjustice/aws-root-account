8332146024c78871c8c74fcf2801ed3b2d2f104f
jakemulley <jakemulley@users.noreply.github.com>
2022-05-30 14:03:16 +0100

    Move Cloud Networks PSN to Closed accounts

--- management-account/terraform/organizations-accounts-closed-accounts.tf
+++ management-account/terraform/organizations-accounts-closed-accounts.tf
@@ -264,3 +264,23 @@ resource "aws_organizations_account" "hmpps_delius_po_test_1" {
     ]
   }
 }
+
+resource "aws_organizations_account" "cloud_networks_psn" {
+  name                       = "Cloud Networks PSN"
+  email                      = replace(local.aws_account_email_addresses_template, "{email}", "cloud_networks_PSN")
+  iam_user_access_to_billing = "ALLOW"
+  parent_id                  = aws_organizations_organizational_unit.closed_accounts.id
+
+  tags = merge(local.tags_technology_services, {
+
+  })
+
+  lifecycle {
+    ignore_changes = [
+      email,
+      iam_user_access_to_billing,
+      name,
+      role_name,
+    ]
+  }
+}
