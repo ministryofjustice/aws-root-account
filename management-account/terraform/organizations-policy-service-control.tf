f555de654444657e43181641fb38d077bf29e0ef
jakemulley <jakemulley@users.noreply.github.com>
2022-06-08 14:25:20 +0100

    Expand SCP for restricted regions to allow reading CloudWatch logs from Network Manager

--- management-account/terraform/organizations-policy-service-control.tf
+++ management-account/terraform/organizations-policy-service-control.tf
@@ -169,7 +169,12 @@ data "aws_iam_policy_document" "deny_non_eu_non_us_east_1_operations" {
   # Deny anything apart from Network Manager in us-west-2
   statement {
     effect      = "Deny"
-    not_actions = ["networkmanager:*"]
+    not_actions = [
+      "networkmanager:*",
+      "cloudwatch:List*", # To view the Network Manager log group
+      "cloudwatch:Get*", # To view the Network Manager log group
+      "cloudwatch:Describe*" # To view the Network Manager log group
+    ]
     resources   = ["*"]
 
     condition {
