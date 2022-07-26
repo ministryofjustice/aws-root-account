43e53db785432abbcfe45ee02474a294b628fd60
jakemulley <jakemulley@users.noreply.github.com>
2022-07-26 21:24:10 +0100

    Remove accounts past their 90-day closure cool-off period

--- management-account/terraform/organizations-accounts-closed-accounts.tf
+++ management-account/terraform/organizations-accounts-closed-accounts.tf
@@ -1,83 +1,9 @@
-resource "aws_organizations_account" "aws_laa" {
-  name                       = "AWS LAA"
-  email                      = replace(local.aws_account_email_addresses_template, "{email}", "laa")
-  iam_user_access_to_billing = "ALLOW"
-  parent_id                  = aws_organizations_organizational_unit.closed_accounts.id
-  tags                       = {}
-
-  lifecycle {
-    ignore_changes = [
-      email,
-      iam_user_access_to_billing,
-      name,
-      role_name,
-    ]
-  }
-}
-
 resource "aws_organizations_account" "money_to_prisoners" {
   name                       = "Money To Prisoners"
   email                      = replace(local.aws_account_email_addresses_template, "{email}", "moneytoprisoners")
   iam_user_access_to_billing = "ALLOW"
   parent_id                  = aws_organizations_organizational_unit.closed_accounts.id
   tags                       = {}
-
-  lifecycle {
-    ignore_changes = [
-      email,
-      iam_user_access_to_billing,
-      name,
-      role_name,
-    ]
-  }
-}
-
-resource "aws_organizations_account" "platforms_non_production" {
-  name                       = "platforms-non-production"
-  email                      = local.aws_account_email_addresses["platforms-non-production"][0]
-  iam_user_access_to_billing = "ALLOW"
-  parent_id                  = aws_organizations_organizational_unit.closed_accounts.id
-  tags                       = {}
-
-  lifecycle {
-    ignore_changes = [
-      email,
-      iam_user_access_to_billing,
-      name,
-      role_name,
-    ]
-  }
-}
-
-resource "aws_organizations_account" "hmpps_delius_po_test" {
-  name                       = "HMPPS Delius PO Test"
-  email                      = replace(local.aws_account_email_addresses_template, "{email}", "hmpps-delius-po-test")
-  iam_user_access_to_billing = "ALLOW"
-  parent_id                  = aws_organizations_organizational_unit.closed_accounts.id
-
-  tags = merge(local.tags_delius, {
-
-  })
-
-  lifecycle {
-    ignore_changes = [
-      email,
-      iam_user_access_to_billing,
-      name,
-      role_name,
-    ]
-  }
-}
-
-resource "aws_organizations_account" "noms_api" {
-  name                       = "NOMS API"
-  email                      = replace(local.aws_account_email_addresses_template, "{email}", "noms-api")
-  iam_user_access_to_billing = "ALLOW"
-  parent_id                  = aws_organizations_organizational_unit.closed_accounts.id
-
-  tags = merge(local.tags_hmpps, {
-
-  })
 
   lifecycle {
     ignore_changes = [
