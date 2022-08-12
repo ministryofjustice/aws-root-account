b74b8744f504d4cd1f043ed4cff95edc18bdcb68
jakemulley <jakemulley@users.noreply.github.com>
2022-08-12 11:06:32 +0100

    Close HMPPS Victim Case Management Integration as no longer used

--- management-account/terraform/organizations-accounts-hmpps-vcms.tf
+++ management-account/terraform/organizations-accounts-hmpps-vcms.tf
@@ -2,26 +2,6 @@ locals {
   tags_victim_case_management_system = local.tags_business_units.hmpps
 }
 
-resource "aws_organizations_account" "hmpps_victim_case_management_system_integration" {
-  name                       = "HMPPS Victim Case Management System Integration"
-  email                      = replace(local.aws_account_email_addresses_template, "{email}", "hmpps-vcms-integration")
-  iam_user_access_to_billing = "ALLOW"
-  parent_id                  = aws_organizations_organizational_unit.hmpps_vcms.id
-
-  tags = merge(local.tags_victim_case_management_system, {
-
-  })
-
-  lifecycle {
-    ignore_changes = [
-      email,
-      iam_user_access_to_billing,
-      name,
-      role_name,
-    ]
-  }
-}
-
 resource "aws_organizations_account" "hmpps_victim_case_management_system_pre_production" {
   name                       = "HMPPS Victim Case Management System Pre Production"
   email                      = replace(local.aws_account_email_addresses_template, "{email}", "hmpps-vcms-pre-prod")
