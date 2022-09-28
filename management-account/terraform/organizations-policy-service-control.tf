bb6ef571197749180c0ef980c54b80d33ed05b6b
julialawrence <julia.lawrence@digital.justice.gov.uk>
2022-09-28 13:24:10 +0100

    Fixing local ref again.

--- management-account/terraform/organizations-policy-service-control.tf
+++ management-account/terraform/organizations-policy-service-control.tf
@@ -325,7 +325,7 @@ data "aws_iam_policy_document" "modernisation_platform_member_ou_scp" {
     condition {
       test     = "StringNotLike"
       variable = "aws:PrincipalARN"
-      values   = ["arn:aws:iam::*:role/OrganizationAccountAccessRole", "arn:aws:iam::${local.modernisation_platform_accounts.modernisation_platform_id}:role/superadmin"]
+      values   = ["arn:aws:iam::*:role/OrganizationAccountAccessRole", "arn:aws:iam::${coalesce(local.modernisation_platform_accounts.modernisation_platform_id...)}:role/superadmin"]
     }
   }
 
@@ -343,7 +343,7 @@ data "aws_iam_policy_document" "modernisation_platform_member_ou_scp" {
     condition {
       test     = "StringNotLike"
       variable = "aws:PrincipalARN"
-      values   = ["arn:aws:iam::*:role/OrganizationAccountAccessRole", "arn:aws:iam::${local.modernisation_platform_accounts.modernisation_platform_id}:role/superadmin"]
+      values   = ["arn:aws:iam::*:role/OrganizationAccountAccessRole", "arn:aws:iam::${coalesce(local.modernisation_platform_accounts.modernisation_platform_id...)}:role/superadmin"]
     }
   }
 }
