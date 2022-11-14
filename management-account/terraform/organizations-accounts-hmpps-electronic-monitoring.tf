a72d02bfd705ee8cd644da26de0d9a00dee44ab6
jakemulley <jakemulley@users.noreply.github.com>
2022-11-14 22:15:40 +0000

    Repurpose Electronic Monitoring Monitoring&Mapping Dev to Electronic Monitoring Acquisitive Crime Training

--- management-account/terraform/organizations-accounts-hmpps-electronic-monitoring.tf
+++ management-account/terraform/organizations-accounts-hmpps-electronic-monitoring.tf
@@ -86,27 +86,6 @@ resource "aws_organizations_account" "electronic_monitoring_infrastructure_dev" 
   }
 }
 
-# Electronic Monitoring Monitoring&Mapping Dev
-resource "aws_organizations_account" "electronic_monitoring_monitoring_and_mapping_dev" {
-  name                       = "Electronic Monitoring Monitoring&Mapping Dev"
-  email                      = replace(local.aws_account_email_addresses_template, "{email}", "hmpps-em-monitoring-mapping-dev")
-  iam_user_access_to_billing = "ALLOW"
-  parent_id                  = aws_organizations_organizational_unit.hmpps_electronic_monitoring.id
-
-  tags = merge(local.tags_electronic_monitoring, {
-
-  })
-
-  lifecycle {
-    ignore_changes = [
-      email,
-      iam_user_access_to_billing,
-      name,
-      role_name,
-    ]
-  }
-}
-
 # Electronic Monitoring Protective Monitoring
 resource "aws_organizations_account" "electronic_monitoring_protective_monitoring" {
   name                       = "Electronic Monitoring Protective Monitoring"
