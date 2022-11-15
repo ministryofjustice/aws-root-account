a994686e20ac35f3ba406d935087a2ac430a8361
Edward Proctor <edward.proctor@digital.justice.gov.uk>
2022-11-15 16:37:39 +0000

    Added networking related changes for the new envs required.

--- management-account/terraform/sso-admin-permission-sets.tf
+++ management-account/terraform/sso-admin-permission-sets.tf
@@ -155,6 +155,7 @@ data "aws_iam_policy_document" "modernisation_platform_developer" {
     actions = [
       "acm:ImportCertificate",
       "autoscaling:UpdateAutoScalingGroup",
+      "dms:CreateEndpoint",
       "lambda:InvokeFunction",
       "lambda:UpdateFunctionCode",
       "secretsmanager:GetResourcePolicy",
