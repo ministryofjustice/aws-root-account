88586a69fc5447fb0ba8813d6f018b1d5d50d3e2
jakemulley <jakemulley@users.noreply.github.com>
2022-12-12 15:40:56 +0000

    Move Cloud Platform Ephemeral Test to closed accounts

--- management-account/terraform/organizations-accounts-closed-accounts.tf
+++ management-account/terraform/organizations-accounts-closed-accounts.tf
@@ -73,3 +73,27 @@ resource "aws_organizations_account" "electronic_monitoring" {
     ]
   }
 }
+
+
+resource "aws_organizations_account" "cloud_platform_ephemeral_test" {
+  name                       = "Cloud Platform Ephemeral Test"
+  email                      = replace(local.aws_account_email_addresses_template, "{email}", "cloud-platform-ephemeral-test")
+  iam_user_access_to_billing = "ALLOW"
+  parent_id                  = aws_organizations_organizational_unit.closed_accounts.id
+
+  tags = merge(local.tags_platforms, {
+    application            = "Cloud Platform Ephemeral Test"
+    environment-name       = "cloud-platform-ephemeral-test"
+    owner                  = "Cloud Platform: platforms@digital.justice.gov.uk"
+    infrastructure-support = "Cloud Platform: platforms@digital.justice.gov.uk"
+  })
+
+  lifecycle {
+    ignore_changes = [
+      email,
+      iam_user_access_to_billing,
+      name,
+      role_name,
+    ]
+  }
+}
--- management-account/terraform/organizations-accounts-platforms-and-architecture-cloud-platform.tf
+++ management-account/terraform/organizations-accounts-platforms-and-architecture-cloud-platform.tf
@@ -21,26 +21,3 @@ resource "aws_organizations_account" "cloud_platform" {
     ]
   }
 }
-
-resource "aws_organizations_account" "cloud_platform_ephemeral_test" {
-  name                       = "Cloud Platform Ephemeral Test"
-  email                      = replace(local.aws_account_email_addresses_template, "{email}", "cloud-platform-ephemeral-test")
-  iam_user_access_to_billing = "ALLOW"
-  parent_id                  = aws_organizations_organizational_unit.platforms_and_architecture_cloud_platform.id
-
-  tags = merge(local.tags_platforms, {
-    application            = "Cloud Platform Ephemeral Test"
-    environment-name       = "cloud-platform-ephemeral-test"
-    owner                  = "Cloud Platform: platforms@digital.justice.gov.uk"
-    infrastructure-support = "Cloud Platform: platforms@digital.justice.gov.uk"
-  })
-
-  lifecycle {
-    ignore_changes = [
-      email,
-      iam_user_access_to_billing,
-      name,
-      role_name,
-    ]
-  }
-}
