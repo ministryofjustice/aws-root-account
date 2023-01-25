39151fe33d51780a725b49cd4edac424a9c72f0e
julialawrence <julia.lawrence@digital.justice.gov.uk>
2023-01-25 12:52:23 +0000

    Renaming analytical platform accounts to conform to a naming standard.

--- management-account/terraform/organizations-accounts-analytical-platform.tf
+++ management-account/terraform/organizations-accounts-analytical-platform.tf
@@ -6,7 +6,7 @@ locals {
 
 # Analytical Platform Data Engineering
 resource "aws_organizations_account" "analytical_platform_data_engineering" {
-  name                       = "Analytical Platform Data Engineering"
+  name                       = "analytical-platform-data-engineering"
   email                      = replace(local.aws_account_email_addresses_template, "{email}", "analytical-platform-data-engineering")
   iam_user_access_to_billing = "ALLOW"
   parent_id                  = aws_organizations_organizational_unit.analytical_platform.id
@@ -27,7 +27,7 @@ resource "aws_organizations_account" "analytical_platform_data_engineering" {
 
 # Analytical Platform Data Engineering Sandbox
 resource "aws_organizations_account" "analytical_platform_data_engineering_sandbox" {
-  name                       = "Analytical Platform Data Engineering Sandbox"
+  name                       = "analytical-platform-data-engineering-sandbox"
   email                      = replace(local.aws_account_email_addresses_template, "{email}", "analytics-platform-dev")
   iam_user_access_to_billing = "ALLOW"
   parent_id                  = aws_organizations_organizational_unit.analytical_platform.id
@@ -48,7 +48,7 @@ resource "aws_organizations_account" "analytical_platform_data_engineering_sandb
 
 # Analytical Platform Development
 resource "aws_organizations_account" "analytical_platform_development" {
-  name                       = "Analytical Platform Development"
+  name                       = "analytical-platform-development"
   email                      = replace(local.aws_account_email_addresses_template, "{email}", "analytical-platform-dev")
   iam_user_access_to_billing = "ALLOW"
   parent_id                  = aws_organizations_organizational_unit.analytical_platform.id
@@ -69,7 +69,7 @@ resource "aws_organizations_account" "analytical_platform_development" {
 
 # Analytical Platform Landing
 resource "aws_organizations_account" "analytical_platform_landing" {
-  name                       = "Analytical Platform Landing"
+  name                       = "analytical-platform-landing"
   email                      = replace(local.aws_account_email_addresses_template, "{email}", "analytical-platform-landing")
   iam_user_access_to_billing = "ALLOW"
   parent_id                  = aws_organizations_organizational_unit.analytical_platform.id
@@ -90,7 +90,7 @@ resource "aws_organizations_account" "analytical_platform_landing" {
 
 # Analytical Platform Production
 resource "aws_organizations_account" "analytical_platform_production" {
-  name                       = "Analytical Platform Production"
+  name                       = "analytical-platform-production"
   email                      = replace(local.aws_account_email_addresses_template, "{email}", "analytical-platform-prod")
   iam_user_access_to_billing = "ALLOW"
   parent_id                  = aws_organizations_organizational_unit.analytical_platform.id
@@ -112,7 +112,7 @@ resource "aws_organizations_account" "analytical_platform_production" {
 
 # MOJ Analytics Platform
 resource "aws_organizations_account" "moj_analytics_platform" {
-  name                       = "MoJ Analytics Platform"
+  name                       = "analytical-platform-data-production"
   email                      = replace(local.aws_account_email_addresses_template, "{email}", "AnalyticsPlatform")
   iam_user_access_to_billing = "ALLOW"
   parent_id                  = aws_organizations_organizational_unit.analytical_platform.id
