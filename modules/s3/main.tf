83b8f7a00037552bfbe16ea9bc10e4094bbe3b74
David Elliott <david.elliott@digital.justice.gov.uk>
2023-08-18 16:58:31 +0100

    Change to match existing values

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -22,7 +22,7 @@ resource "aws_s3_bucket_acl" "default" {
 resource "aws_s3_bucket_ownership_controls" "default" {
   bucket = aws_s3_bucket.default.id
   rule {
-    object_ownership = "BucketOwnerPreferred"
+    object_ownership = "ObjectWriter"
   }
 }
 
