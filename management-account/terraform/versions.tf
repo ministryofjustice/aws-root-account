d8b119d8156db81ce8d3cc6f84df8adecce25510
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2023-09-06 15:01:01 +0000

    Remove Common Fate
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- management-account/terraform/versions.tf
+++ management-account/terraform/versions.tf
@@ -5,14 +5,5 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
-    commonfate = {
-      source  = "common-fate/commonfate"
-      version = "1.2.0"
-    }
   }
-}
-
-provider "commonfate" {
-  aws_region         = "eu-west-2"
-  governance_api_url = module.commonfate.governance_rest_api_endpoint
 }
