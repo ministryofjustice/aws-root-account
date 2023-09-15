4c7a75bf124d140ec7bcf24a2e7b9c4275bbe4f3
David Elliott <david.elliott@digital.justice.gov.uk>
2023-09-15 21:00:25 +0100

    Update to add ccms-ebs ou to oracle license

--- organisation-security/terraform/locals.tf
+++ organisation-security/terraform/locals.tf
@@ -74,12 +74,14 @@ locals {
     if ou.name == "LAA"
   ]...)
 
-  # for license manager testing
-  ou_example = coalesce([
-    for ou in data.aws_organizations_organizational_units.modernisation_platform_member.children :
-    ou.id
-    if ou.name == "modernisation-platform-example"
-  ]...)
+  # ous for license manager
+  ou_example = coalesce([for ou in data.aws_organizations_organizational_units.modernisation_platform_member.children : ou.id if ou.name == "modernisation-platform-example"]...)
+  ou_ccms_ebs = coalesce([for ou in data.aws_organizations_organizational_units.modernisation_platform_member.children : ou.id if ou.name == "modernisation-platform-ccms-ebs"]...)
+
+  license_mamager_ous = [
+    local.ou_example,
+    local.ou_ccms_ebs
+  ]
 
   # modernisation_platform_member_ous = [
   #   for ou in data.aws_organizations_organizational_units.modernisation_platform_member.children :
