4bfa34b4e26cac9dda88ee74c46966f1de9472a4
jakemulley <jakemulley@users.noreply.github.com>
2023-09-20 11:20:27 +0100

    legal-aid-agency: move sandbox account to closed, as no longer used

--- management-account/terraform/organizations-accounts-laa.tf
+++ management-account/terraform/organizations-accounts-laa.tf
@@ -127,21 +127,3 @@ resource "aws_organizations_account" "laa_uat" {
     ]
   }
 }
-
-resource "aws_organizations_account" "legal_aid_agency" {
-  name                       = "Legal Aid Agency"
-  email                      = replace(local.aws_account_email_addresses_template, "{email}", "legalaid")
-  iam_user_access_to_billing = "ALLOW"
-  parent_id                  = aws_organizations_organizational_unit.laa.id
-
-  tags = local.tags_laa
-
-  lifecycle {
-    ignore_changes = [
-      email,
-      iam_user_access_to_billing,
-      name,
-      role_name,
-    ]
-  }
-}
