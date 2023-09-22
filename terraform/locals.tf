4accb8cf24a73cb50ac7bfefeed49504cde0a68b
David Elliott <david.elliott@digital.justice.gov.uk>
2023-09-22 10:43:46 +0100

    Remove org sec account from root terraform
    This account has already been migrated but was left here because of
    dependancies.  These have now been removed and the remaining
    dependancies resolved by retrieving the organisation security account
    number via an org data source.  Once Guard duty has been moved the org
    sec providers and this data source can also be removed.

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,5 +1,7 @@
 data "aws_caller_identity" "current" {}
 
+data "aws_organizations_organization" "default" {}
+
 locals {
   tags-business-unit = {
     hq = {
@@ -38,4 +40,17 @@ locals {
   })
   caller_identity   = data.aws_caller_identity.current
   github_repository = "github.com/ministryofjustice/aws-root-account/blob/main/terraform"
+
+  tags-organisation-management = {
+    application            = "Organisation Management"
+    business-unit          = "Platforms"
+    infrastructure-support = "Hosting Leads: hosting-leads@digital.justice.gov.uk"
+    is-production          = true
+    owner                  = "Hosting Leads: hosting-leads@digital.justice.gov.uk"
+    source-code            = "github.com/ministryofjustice/aws-root-account"
+  }
+
+  organisation_security_name = "organisation-security"
+  org_sec_acc_index          = index(data.aws_organizations_organization.default.accounts[*].name, local.organisation_security_name)
+  organisation_security_id   = data.aws_organizations_organization.default.accounts[local.org_sec_acc_index].id
 }
