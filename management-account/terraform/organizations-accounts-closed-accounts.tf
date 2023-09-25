8ad51e3bed3def879852ff568f57fc3bbb34cf81
jakemulley <jakemulley@users.noreply.github.com>
2023-09-25 12:18:20 +0100

    Remove MOJ Info Services Dev account, as no longer used

--- management-account/terraform/organizations-accounts-closed-accounts.tf
+++ management-account/terraform/organizations-accounts-closed-accounts.tf
@@ -16,21 +16,3 @@ resource "aws_organizations_account" "legal_aid_agency" {
     ]
   }
 }
-
-resource "aws_organizations_account" "moj_info_services_dev" {
-  name                       = "MoJ Info Services Dev"
-  email                      = replace(local.aws_account_email_addresses_template, "{email}", "courtfinder")
-  iam_user_access_to_billing = "ALLOW"
-  parent_id                  = aws_organizations_organizational_unit.closed_accounts.id
-
-  tags = local.tags_hmcts
-
-  lifecycle {
-    ignore_changes = [
-      email,
-      iam_user_access_to_billing,
-      name,
-      role_name,
-    ]
-  }
-}
