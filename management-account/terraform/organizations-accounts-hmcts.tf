bfb1679376afe1be8141c6a0b7ef9ee6ffe772f5
jakemulley <jakemulley@users.noreply.github.com>
2023-10-03 13:35:55 +0100

    hmcts-fee-remissions: delete account as unused

--- management-account/terraform/organizations-accounts-hmcts.tf
+++ management-account/terraform/organizations-accounts-hmcts.tf
@@ -2,28 +2,6 @@ locals {
   tags_hmcts = local.tags_business_units.hmcts
 }
 
-# Used to contain Help with Fees, Tax Tribunals, awaiting confirmation of DNS removal before deletion
-resource "aws_organizations_account" "hmcts_fee_remissions" {
-  name                       = "HMCTS Fee Remissions"
-  email                      = replace(local.aws_account_email_addresses_template, "{email}", "fee-remission")
-  iam_user_access_to_billing = "ALLOW"
-  parent_id                  = aws_organizations_organizational_unit.hmcts.id
-
-  tags = merge(local.tags_hmcts, {
-    application   = "Fee Remissions",
-    is-production = true
-  })
-
-  lifecycle {
-    ignore_changes = [
-      email,
-      iam_user_access_to_billing,
-      name,
-      role_name,
-    ]
-  }
-}
-
 # Contains Make a Plea - migrating October 2023
 resource "aws_organizations_account" "manchester_traffic_dev" {
   name                       = "Manchester Traffic Dev"
