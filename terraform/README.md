5a22097cc06952040b28a6271fb3bf48d1f9e7d6
David Elliott <david.elliott@digital.justice.gov.uk>
2023-10-10 17:12:49 +0100

    Remove root Terraform folder
    All resources have now been migrated to the management account or
    organisation security terraform, this terraform now runs clean with no
    resources in it and can be removed.

--- terraform/README.md
+++
@@ -1,2 +0,0 @@
-# AWS Root Account Infrastructure
-This folder contains the infrastructure as code, in Terraform, to recreate and administer the AWS root account.
--- terraform/main.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  # `backend` blocks do not support variables, so the bucket name is hard-coded here, although created in s3.tf
-  backend "s3" {
-    bucket  = "moj-aws-root-account-terraform-state"
-    region  = "eu-west-2"
-    key     = "root/terraform.tfstate"
-    encrypt = true
-  }
-}
