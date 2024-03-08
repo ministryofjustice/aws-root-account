66fdbd6db66adb4848f0d302d87a2bdbb4cd7374
Antony Bishop <36888942+AntonyBishop@users.noreply.github.com>
2024-03-08 11:06:41 +0000

    Add moj-eucs-identity Team to SSO

--- management-account/terraform/sso-admin-account-assignments.tf
+++ management-account/terraform/sso-admin-account-assignments.tf
@@ -292,6 +292,17 @@ locals {
       ]
     },
     {
+      github_team        = "moj-eucs-identity",
+      permission_set_arn = aws_ssoadmin_permission_set.read_only_access.arn,
+      account_ids = [
+        aws_organizations_account.moj_official_development.id,
+        aws_organizations_account.moj_official_preproduction.id,
+        aws_organizations_account.moj_official_production.id,
+        aws_organizations_account.moj_official_public_key_infrastructure_dev.id,
+        aws_organizations_account.moj_official_public_key_infrastructure.id
+      ]
+    },
+    {
       github_team        = "modernisation-platform-engineers",
       permission_set_arn = aws_ssoadmin_permission_set.aws_sso_read_only.arn,
       account_ids = [
