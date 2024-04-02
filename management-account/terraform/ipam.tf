64b8e8650d3f58acb20f6b57c6baf4218a29e036
David Elliott <david.elliott@digital.justice.gov.uk>
2024-04-02 16:10:51 +0100

    Add IPAM and scopes
    Also adding eu-central-1 (used for Bedrock).
    
    This IPAM and scope alread exists, just adding to code.

--- management-account/terraform/ipam.tf
+++ management-account/terraform/ipam.tf
@@ -13,3 +13,8 @@ resource "aws_vpc_ipam_organization_admin_account" "eu_west_1" {
   provider                   = aws.eu-west-1
   delegated_admin_account_id = aws_organizations_account.organisation_security.id
 }
+
+resource "aws_vpc_ipam_organization_admin_account" "eu_central_1" {
+  provider                   = aws.eu-central-1
+  delegated_admin_account_id = aws_organizations_account.organisation_security.id
+}
---
+++ organisation-security/terraform/ipam.tf
@@ -0,0 +1,33 @@
+###############################
+# IPAM in EU regions #
+###############################
+
+locals {
+  ipam_regions = [
+    "eu-west-1",
+    "eu-west-2",
+    "eu-central-1"
+  ]
+}
+
+# Create IPAM
+resource "aws_vpc_ipam" "main" {
+  description = "Multi Region IPAM"
+  dynamic operating_regions {
+    for_each = local.ipam_regions
+    content {
+      region_name = operating_regions.value
+    }
+  }
+}
+
+# Create Scopes
+resource "aws_vpc_ipam_scope" "public" {
+  ipam_id     = aws_vpc_ipam.main.id
+  description = "Public Scope"
+}
+
+resource "aws_vpc_ipam_scope" "private" {
+  ipam_id     = aws_vpc_ipam.main.id
+  description = "Private Scope"
+}
