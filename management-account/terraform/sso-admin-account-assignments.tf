214e3177a52f73dbb5ad5fac738bd13949e8da40
David Sibley <david.sibley@digital.justice.gov.uk>
2024-06-13 12:22:39 +0100

    added Organisation Security account to view only for mod platform engineers

--- management-account/terraform/sso-admin-account-assignments.tf
+++ management-account/terraform/sso-admin-account-assignments.tf
@@ -219,6 +219,7 @@ locals {
       account_ids = [
         aws_organizations_account.moj_official_production.id,
         aws_organizations_account.moj_official_shared_services.id,
+        aws_organizations_account.organisation_security.id
       ]
     },
     {
