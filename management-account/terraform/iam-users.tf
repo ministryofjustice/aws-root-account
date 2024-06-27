2bdb7412c0bbd1a2ffd9fa3ffce39571555cdebf
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2024-06-27 13:12:30 +0100

    :fire: Remove Poornima Krishnasamy account
    This user has left the service and as such should have her account revoked.

--- management-account/terraform/iam-users.tf
+++ management-account/terraform/iam-users.tf
@@ -99,27 +99,6 @@ resource "aws_iam_user_group_membership" "modernisation_platform_organisation_ma
   ]
 }
 
-########################
-# Poornima Krishnasamy #
-########################
-resource "aws_iam_user" "poornima_krishnasamy" {
-  name          = "Poornima.Krishnasamy"
-  path          = "/"
-  force_destroy = true
-  tags          = {}
-}
-
-# User membership
-resource "aws_iam_user_group_membership" "poornima_krishnasamy" {
-  user = aws_iam_user.poornima_krishnasamy.name
-
-  groups = [
-    aws_iam_group.admin.name,
-    aws_iam_group.aws_organisations_admin.name,
-    aws_iam_group.billing_full_access.name,
-  ]
-}
-
 ##############
 # Sablu Miah #
 ##############
