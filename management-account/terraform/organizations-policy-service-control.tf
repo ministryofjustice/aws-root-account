49b4d0c5c802dded445ac2fa0209322e57620fc8
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2024-07-23 11:36:15 +0100

    🔥 Remove DSO OU (#944)

--- management-account/terraform/organizations-policy-service-control.tf
+++ management-account/terraform/organizations-policy-service-control.tf
@@ -240,7 +240,6 @@ locals {
     aws_organizations_organizational_unit.laa.id,
     aws_organizations_organizational_unit.opg.id,
     aws_organizations_organizational_unit.platforms_and_architecture_cloud_platform.id,
-    aws_organizations_organizational_unit.platforms_and_architecture_digital_studio_operations.id,
     aws_organizations_organizational_unit.platforms_and_architecture_modernisation_platform.id,
     aws_organizations_organizational_unit.platforms_and_architecture_operations_engineering.id,
     aws_organizations_organizational_unit.security_engineering.id,
