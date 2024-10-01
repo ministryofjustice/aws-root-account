dacfbc377bf1dac4738877a27f6e2050d89b9ccb
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-10-01 13:05:52 +0100

    Add Athena enabled CUR

--- management-account/terraform/cost-and-usage-reports.tf
+++ management-account/terraform/cost-and-usage-reports.tf
@@ -33,3 +33,21 @@ resource "aws_cur_report_definition" "quicksight_integration" {
   s3_region = "eu-west-2"
   s3_prefix = "CUR"
 }
+
+resource "aws_cur_report_definition" "athena_integration" {
+  provider = aws.us-east-1
+
+  report_name                = "MOJ-CUR-ATHENA"
+  time_unit                  = "DAILY"
+  format                     = "textORcsv"
+  compression                = "ZIP"
+  additional_schema_elements = ["RESOURCES", "SPLIT_COST_ALLOCATION_DATA"]
+  additional_artifacts       = ["ATHENA"]
+  refresh_closed_reports     = true
+  report_versioning          = "OVERWRITE_REPORT"
+
+  # S3 configuration
+  s3_bucket = module.cur_reports_s3_bucket.bucket_name
+  s3_region = "eu-west-2"
+  s3_prefix = "CUR-ATHENA"
+}
