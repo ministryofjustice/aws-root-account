9bd5f898b2e9875298b68eef05dd6deb30f7fddb
David Elliott <david.elliott@digital.justice.gov.uk>
2024-10-04 15:22:38 +0100

    Add dynamoDB put item for state lock table for mp eng role
    Allowing the mp engineer role to write to dynamodb to aquire a state lock.

--- management-account/terraform/sso-admin-permission-sets.tf
+++ management-account/terraform/sso-admin-permission-sets.tf
@@ -333,6 +333,12 @@ data "aws_iam_policy_document" "modernisation_platform_engineer" {
     ]
     resources = ["*"]
   }
+  statement {
+    sid    = "AllowStateLock"
+    effect = "Allow"
+    actions = ["dynamodb:PutItem"]
+    resources = ["arn:aws:dynamodb:eu-west-2:${coalesce(local.modernisation_platform_accounts.modernisation_platform_id...)}:table/modernisation-platform-terraform-state-lock"]
+  }
 }
 
 # Modernisation Platform end user permission sets are now managed in the modernisation-platform repository
