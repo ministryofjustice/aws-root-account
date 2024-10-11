c50005dff38b6834af1ae18cbf67789e7e6d40c0
Aaron Robinson <41325732+ASTRobinson@users.noreply.github.com>
2024-10-11 13:31:05 +0100

    Add S3 Replication KMS functionality (#1013)
    * S3 Replication KMS functionality
    
    * fix megalinter fmt warnings
    
    * fix megalinter fmt warnings 2
    
    * fix megalinter fmt warnings 3

--- management-account/terraform/s3.tf
+++ management-account/terraform/s3.tf
@@ -194,11 +194,16 @@ module "cur_reports_s3_bucket" {
   enable_replication     = true
   replication_bucket_arn = "arn:aws:s3:::moj-cur-reports-modplatform-20240930164810837800000001"
   replication_role_arn   = module.cur_reports_s3_bucket.replication_role_arn
+  source_kms_arn         = "arn:aws:kms:*:${data.aws_caller_identity.current.account_id}:alias/aws/s3"
+  destination_kms_arn    = "arn:aws:kms:eu-west-2:${coalesce(local.modernisation_platform_accounts.core_logging_id...)}:alias/moj-cur-reports-key"
   replication_rules = [
     {
-      id     = "replicate-cur-athena"
-      prefix = "CUR-ATHENA/"
-      status = "Enabled"
+      id                 = "replicate-cur-athena"
+      prefix             = "CUR-ATHENA/"
+      status             = "Enabled"
+      deletemarker       = "Enabled"
+      replica_kms_key_id = "arn:aws:kms:eu-west-2:${coalesce(local.modernisation_platform_accounts.core_logging_id...)}:alias/moj-cur-reports-key"
+      metrics            = "Enabled"
     }
   ]
 }
