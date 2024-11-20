b71c5f5fc7ccd4397c85701da5bd6fecca51b9cb
Mark Roberts <mark.roberts@digital.justice.gov.uk>
2024-11-20 11:54:18 +0000

    change of s3 bucket

--- organisation-security/terraform/license-manager.tf
+++ organisation-security/terraform/license-manager.tf
@@ -45,7 +45,7 @@ module "oracle_ec2_license_configurations" {
 
 # Upload the YAML file to the existing S3 bucket
 resource "aws_s3_object" "oracle_db_lts_orch" {
-  bucket = "license-manager-artifact-bucket" # Existing S3 bucket name
+  bucket = "aws-license-manager-service-643d94b3-abff-46cd-as" # Existing S3 bucket name
   key    = "OracleDbLTS-Orch.yaml"
   source = "./cloudformation/OracleDbLTS-Orch.yaml"
   acl    = "private"
@@ -68,7 +68,7 @@ resource "aws_cloudformation_stack" "oracleblts" {
     MaxErrors                = 4
     Schedule                 = "cron(15 0 ? * MON *)"
   }
-  template_url = "https://license-manager-artifact-bucket.s3.eu-west-2.amazonaws.com/OracleDbLTS-Orch.yaml"
+  template_url = "https://aws-license-manager-service-643d94b3-abff-46cd-as.s3.eu-west-2.amazonaws.com/OracleDbLTS-Orch.yaml"
 
   depends_on = [
     module.oracle_ec2_license_configurations,
