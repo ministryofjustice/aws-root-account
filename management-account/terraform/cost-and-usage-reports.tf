40ae498d2f7109578c7633c50ace54545837191c
Kudzai Mtoko <kudzai.mtoko@digital.justice.gov.uk>
2025-01-15 16:34:37 +0000

    test

--- management-account/terraform/cost-and-usage-reports.tf
+++ management-account/terraform/cost-and-usage-reports.tf
@@ -64,7 +64,7 @@ resource "aws_cur_report_definition" "moj_cur_report" {
   report_versioning          = "OVERWRITE_REPORT"
 
   # S3 configuration
-  s3_bucket = module.cur_reports_greenopspoc_s3_bucket.bucket_name
+  s3_bucket = module.cur_reports_greenopspoc_s3_bucket.bucket.name
   s3_region = "eu-west-2"
   s3_prefix = "moj-cost-and-usage-reports/"
 }
