1bfeeac7a75c50e95d6a9506bf054995e1d03f98
Mark Roberts <44971873+markgov@users.noreply.github.com>
2025-01-29 10:43:57 +0000

    Update iam.tf

--- management-account/terraform/iam.tf
+++ management-account/terraform/iam.tf
@@ -11,7 +11,7 @@ resource "aws_iam_account_alias" "default" {
 resource "aws_iam_account_password_policy" "default" {
   allow_users_to_change_password = true
   hard_expiry                    = false
-  max_password_age               = 100
+  max_password_age               = 90
   minimum_password_length        = 14
   password_reuse_prevention      = 24
   require_lowercase_characters   = true
