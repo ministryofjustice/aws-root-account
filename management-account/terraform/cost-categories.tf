b40ba3d7e2ce6a159a13bf68da9094e56fff8f6d
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2025-01-31 16:23:41 +0000

    ✨ Add Modernisation Platform Cost Category (#1087)

---
+++ management-account/terraform/cost-categories.tf
@@ -0,0 +1,19 @@
+data "aws_organizations_organizational_unit_descendant_accounts" "modernisation_platform" {
+  parent_id = aws_organizations_organizational_unit.platforms_and_architecture_modernisation_platform.id
+}
+
+resource "aws_ce_cost_category" "modernisation_platform" {
+  name            = "Modernisation Platform"
+  rule_version    = "CostCategoryExpression.v1"
+  effective_start = "2024-01-01T00:00:00Z"
+  rule {
+    rule {
+      dimension {
+        key           = "LINKED_ACCOUNT"
+        values        = data.aws_organizations_organizational_unit_descendant_accounts.modernisation_platform.accounts[*].id
+        match_options = ["EQUALS"]
+      }
+    }
+  }
+}
+
