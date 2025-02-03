23b877813ed8e2b77a60cd206b24bdc9b1e1d857
connormaglynn <connor.glynn@digital.justice.gov.uk>
2025-02-03 09:36:43 +0000

    🔧 Fix Effective Start Date Being Longer than 12 Months

--- management-account/terraform/cost-categories.tf
+++ management-account/terraform/cost-categories.tf
@@ -5,8 +5,9 @@ data "aws_organizations_organizational_unit_descendant_accounts" "modernisation_
 resource "aws_ce_cost_category" "modernisation_platform" {
   name            = "Modernisation Platform"
   rule_version    = "CostCategoryExpression.v1"
-  effective_start = "2024-01-01T00:00:00Z"
+  effective_start = "2024-02-01T00:00:00Z"
   rule {
+    type  = "REGULAR"
     value = "Modernisation Platform"
     rule {
       dimension {
