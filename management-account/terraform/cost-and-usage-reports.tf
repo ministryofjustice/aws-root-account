5fe99aa53b092684379f21bfb8348a7129ee6145
Kudzai Mtoko <kudzai.mtoko@digital.justice.gov.uk>
2025-02-07 10:34:49 +0000

    #8739-bucket-cur-update-prod

--- management-account/terraform/cost-and-usage-reports.tf
+++ management-account/terraform/cost-and-usage-reports.tf
@@ -56,15 +56,16 @@ resource "aws_cur_report_definition" "athena_integration" {
 resource "aws_cur_report_definition" "moj_cur_report" {
   provider = aws.us-east-1
 
-  report_name                = "MOJ-CUR-GREENOPSPOC"
+  report_name                = "MOJ-CUR-V2-HOURLY"
   time_unit                  = "HOURLY"
   format                     = "Parquet"
   compression                = "Parquet"
-  additional_schema_elements = ["RESOURCES"]
+  additional_schema_elements = ["RESOURCES", "SPLIT_COST_ALLOCATION_DATA"]
+  additional_artifacts       = ["ATHENA"]
   report_versioning          = "OVERWRITE_REPORT"
 
   # S3 configuration
-  s3_bucket = module.cur_reports_greenopspoc_s3_bucket.bucket.bucket
+  s3_bucket = module.cur_reports_v2_hourly_s3_bucket.bucket_name
   s3_region = "eu-west-2"
   s3_prefix = "moj-cost-and-usage-reports/"
 }
