5bdc092c2087ce7ea13215ae797a604bcf98c0e3
connormaglynn <connor.glynn@digital.justice.gov.uk>
2025-03-03 12:57:48 +0000

    ✨ Import Cloud Platform Cost Category

---
+++ management-account/terraform/cost-categories-cloud-platform.tf
@@ -0,0 +1,21 @@
+resource "aws_ce_cost_category" "cloud_platform" {
+  name            = "Cloud Platform"
+  rule_version    = "CostCategoryExpression.v1"
+  effective_start = "2024-01-01T00:00:00Z"
+  rule {
+    type  = "REGULAR"
+    value = "Cloud Platform"
+    rule {
+      dimension {
+        key           = "LINKED_ACCOUNT"
+        values        = data.aws_organizations_organizational_unit_descendant_accounts.cloud_platform.accounts[*].id
+        match_options = ["EQUALS"]
+      }
+    }
+  }
+}
+
+import {
+  to = aws_ce_cost_category.cloud_platform
+  id = "arn:aws:ce::${data.aws_caller_identity.current.account_id}:costcategory/3edadf90-8c24-467b-a80a-f5e96df1beb2"
+}
