a1f115c1e04057cd184de070692f190ef8f9b540
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2025-03-04 10:33:27 +0000

    ✨ Add Recent Effective Start Date to Modernisation Platform and Business Unit (#1139)

--- management-account/terraform/cost-categories-modernisation-platform.tf
+++ management-account/terraform/cost-categories-modernisation-platform.tf
@@ -1,6 +1,7 @@
 resource "aws_ce_cost_category" "modernisation_platform" {
-  name         = "Modernisation Platform"
-  rule_version = "CostCategoryExpression.v1"
+  name            = "Modernisation Platform"
+  rule_version    = "CostCategoryExpression.v1"
+  effective_start = "2025-02-01T00:00:00Z"
 
   rule {
     type  = "REGULAR"
