69def89619b7bfe8c028df71140340dce88c1a18
connormaglynn <connor.glynn@digital.justice.gov.uk>
2025-03-05 20:54:38 +0000

    🔥 Remove Unused Data Source

--- management-account/terraform/cost-categories-data.tf
+++ management-account/terraform/cost-categories-data.tf
@@ -1,5 +1,3 @@
-data "aws_organizations_organization" "all" {}
-
 data "aws_organizations_organizational_unit_descendant_accounts" "modernisation_platform" {
   parent_id = aws_organizations_organizational_unit.platforms_and_architecture_modernisation_platform.id
 }
