1b64c04f14b284845f3e81d0600b74fafaee5be5
David Sibley <david.sibley@digital.justice.gov.uk>
2025-04-03 12:43:23 +0100

    Create bucket for storage of XDR/XSIAM templates

--- management-account/terraform/s3.tf
+++ management-account/terraform/s3.tf
@@ -383,3 +383,10 @@ data "aws_iam_policy_document" "cur_reports_v2_hourly_s3_policy" {
     }
   }
 }
+
+# moj-xdr-template-storage
+module "xdr_template_storage" {
+  source          = "../../modules/s3"
+  additional_tags = local.root_account
+  bucket_prefix   = "xdr_template_storage"
+}
