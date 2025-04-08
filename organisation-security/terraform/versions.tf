a4c0a62a214f262a5976b413617c31864e02ea4a
dms1981 <david.sibley@digital.justice.gov.uk>
2025-04-08 15:06:31 +0100

    Revert "Create CloudFormation stack & stackset for Palo Alto Cortex"

--- organisation-security/terraform/versions.tf
+++ organisation-security/terraform/versions.tf
@@ -5,9 +5,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.6.3"
+      version = "3.6.3"
     }
   }
 }
