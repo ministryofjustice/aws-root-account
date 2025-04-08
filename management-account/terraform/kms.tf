f32500d4d8659ad91b30105d632024791dbb0382
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2025-04-08 14:34:53 +0100

    remove kms key

--- management-account/terraform/kms.tf
+++ management-account/terraform/kms.tf
@@ -37,19 +37,3 @@ data "aws_iam_policy_document" "terraform_state_s3_bucket_kms" {
     }
   }
 }
-
-module "focus_s3_kms" {
-  #checkov:skip=CKV_TF_1:Module registry does not support commit hashes for versions
-  #checkov:skip=CKV_TF_2:Module registry does not support tags for versions
-
-  source  = "terraform-aws-modules/kms/aws"
-  version = "3.1.1"
-
-  aliases               = ["s3/focus"]
-  description           = "S3 FOCUS KMS key"
-  enable_default_policy = true
-
-  deletion_window_in_days = 7
-
-  tags = local.tags
-}
