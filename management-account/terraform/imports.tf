28b4c2c92d381518dccdbd9a89e9bcba58957134
julialawrence <julia.lawrence@digital.justice.gov.uk>
2025-04-10 15:47:29 +0100

    Importing created resources and deleting problematic ones

---
+++ management-account/terraform/imports.tf
@@ -0,0 +1,9 @@
+import {
+  to = aws_ssoadmin_account_assignment.github_team_access["cloud-platform-engineers-754256621582-arn:aws:sso:::permissionSet/ssoins-7535d9af4f41fb26/ps-4fe7bf47e9790ed6"]
+  id = "664252b4-7021-701e-49b9-6c46ccc7899e,GROUP,754256621582,AWS_ACCOUNT,arn:aws:sso:::permissionSet/ssoins-7535d9af4f41fb26/ps-4fe7bf47e9790ed6,arn:aws:sso:::instance/ssoins-7535d9af4f41fb26"
+}
+
+import {
+  to = aws_ssoadmin_account_assignment.github_team_access["webops-754256621582-arn:aws:sso:::permissionSet/ssoins-7535d9af4f41fb26/ps-314024a5720eec87"]
+  id = "9c6710dd7f-0e43e021-1707-4fea-ab53-3b9eb9cf785c,GROUP,754256621582,AWS_ACCOUNT,arn:aws:sso:::permissionSet/ssoins-7535d9af4f41fb26/ps-314024a5720eec87,arn:aws:sso:::instance/ssoins-7535d9af4f41fb26"
+}
