e6e8bc94a34450cbe70db355ed0e340315e51585
Kudzai-moj <kudzai.mtoko@digital.justice.gov.uk>
2025-05-02 10:42:53 +0100

    made resource arn dynamic

--- management-account/terraform/sso-admin-permission-sets.tf
+++ management-account/terraform/sso-admin-permission-sets.tf
@@ -433,7 +433,9 @@ data "aws_iam_policy_document" "modernisation_platform_engineer" {
       "organizations:CloseAccount",
       "organizations:MoveAccount"
     ]
-    resources = ["arn:aws:organizations::295814833350:ou/o-o-b2fpbzyd95/ou-ou-j1kx-qxsrh1gv"]
+    resources = ["arn:aws:organizations::${data.aws_caller_identity.current.account_id}:ou/${aws_organizations_organization.default.id}/${aws_organizations_organizational_unit.platforms_and_architecture_modernisation_platform.id}"]
+
+
   }
   statement {
     effect = "Allow"
