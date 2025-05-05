3c36fa119791f89316b75d27bfc232c09dd957a6
Kudzai-moj <kudzai.mtoko@digital.justice.gov.uk>
2025-05-05 19:51:35 +0100

    removed sid visual editor

--- management-account/terraform/sso-admin-permission-sets.tf
+++ management-account/terraform/sso-admin-permission-sets.tf
@@ -425,7 +425,6 @@ data "aws_iam_policy_document" "modernisation_platform_engineer" {
     resources = ["arn:aws:dynamodb:eu-west-2:${coalesce(local.modernisation_platform_accounts.modernisation_platform_id...)}:table/modernisation-platform-terraform-state-lock"]
   }
   statement {
-    sid    = "VisualEditor0"
     effect = "Allow"
     actions = [
       "organizations:RemoveAccountFromOrganization",
