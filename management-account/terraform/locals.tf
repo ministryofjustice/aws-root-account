934b92e85894863b51cae3c9013d961a58dc42e1
julialawrence <julia.lawrence@digital.justice.gov.uk>
2025-05-06 13:41:14 +0100

    Trying to trigger checks.

--- management-account/terraform/locals.tf
+++ management-account/terraform/locals.tf
@@ -134,6 +134,7 @@ locals {
   ]
   # LAA Data Location locals
 
+
   laa_lz_data_locations = try(jsondecode(aws_secretsmanager_secret_version.laa_lz_data_locations.secret_string).locations, [])
   laa_lz_data_locations_resources = flatten([
     for location in local.laa_lz_data_locations : [
