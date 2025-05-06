ac8bc357bbd03ba0f94db7ae0143e22ea2051c06
julialawrence <julia.lawrence@digital.justice.gov.uk>
2025-05-06 14:32:50 +0100

    access has 2 ss's not 1

--- management-account/terraform/sso-admin-account-assignments.tf
+++ management-account/terraform/sso-admin-account-assignments.tf
@@ -454,7 +454,7 @@ locals {
     },
     {
       github_team        = "azure-aws-sso-laa-readers",
-      permission_set_arn = aws_ssoadmin_permission_set.laa_lz_s3_read_acces.arn,
+      permission_set_arn = aws_ssoadmin_permission_set.laa_lz_s3_read_access.arn,
       account_ids = [
         aws_organizations_account.laa_production.id,
       ]
