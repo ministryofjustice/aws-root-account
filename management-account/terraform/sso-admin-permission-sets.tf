540a982a1dae41b40d7ba2f11538316a6557c9dc
Kudzai-moj <kudzai.mtoko@digital.justice.gov.uk>
2025-05-08 10:53:02 +0100

    added Mot Plat Sid

--- management-account/terraform/sso-admin-permission-sets.tf
+++ management-account/terraform/sso-admin-permission-sets.tf
@@ -425,7 +425,7 @@ data "aws_iam_policy_document" "modernisation_platform_engineer" {
     resources = ["arn:aws:dynamodb:eu-west-2:${coalesce(local.modernisation_platform_accounts.modernisation_platform_id...)}:table/modernisation-platform-terraform-state-lock"]
   }
   statement {
-    sid    = "VisualEditor0"
+    sid    = "ManageModernisationPlatformAccounts"
     effect = "Allow"
     actions = [
       "organizations:RemoveAccountFromOrganization",
