6a77201b429723379b2ae280ef121efb30078f1a
Kudzai-moj <kudzai.mtoko@digital.justice.gov.uk>
2025-05-08 09:59:00 +0100

    updated code

--- management-account/terraform/sso-admin-permission-sets.tf
+++ management-account/terraform/sso-admin-permission-sets.tf
@@ -415,6 +415,7 @@ data "aws_iam_policy_document" "modernisation_platform_engineer" {
     ]
     resources = ["*"]
   }
+
   statement {
     sid    = "AllowStateLock"
     effect = "Allow"
@@ -424,7 +425,9 @@ data "aws_iam_policy_document" "modernisation_platform_engineer" {
     ]
     resources = ["arn:aws:dynamodb:eu-west-2:${coalesce(local.modernisation_platform_accounts.modernisation_platform_id...)}:table/modernisation-platform-terraform-state-lock"]
   }
+
   statement {
+    sid    = "VisualEditor0"
     effect = "Allow"
     actions = [
       "organizations:RemoveAccountFromOrganization",
@@ -433,9 +436,8 @@ data "aws_iam_policy_document" "modernisation_platform_engineer" {
       "organizations:MoveAccount"
     ]
     resources = ["arn:aws:organizations::${data.aws_caller_identity.current.account_id}:ou/${aws_organizations_organization.default.id}/${aws_organizations_organizational_unit.platforms_and_architecture_modernisation_platform.id}"]
-
-
   }
+
   statement {
     effect = "Allow"
     actions = [
