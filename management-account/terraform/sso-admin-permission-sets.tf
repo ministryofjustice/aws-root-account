8da4ccfa3e169bfd996cb6d057849cdd46dd7a15
Kudzai-moj <kudzai.mtoko@digital.justice.gov.uk>
2025-05-09 19:30:00 +0100

    Updates with notes

--- management-account/terraform/sso-admin-permission-sets.tf
+++ management-account/terraform/sso-admin-permission-sets.tf
@@ -424,6 +424,26 @@ data "aws_iam_policy_document" "modernisation_platform_engineer" {
     ]
     resources = ["arn:aws:dynamodb:eu-west-2:${coalesce(local.modernisation_platform_accounts.modernisation_platform_id...)}:table/modernisation-platform-terraform-state-lock"]
   }
+  statement {
+    sid    = "ModPlatAccountsAllowMoveFromSpecificOUBranchToRoot"
+    effect = "Allow"
+    actions = [
+      "organizations:MoveAccount"
+    ]
+    resources = [
+      // move will only succeed if the source OU is authorized below.
+      "arn:aws:organizations::${data.aws_caller_identity.current.account_id}:account/${aws_organizations_organization.default.id}/*",
+
+      // specific source OU we are allowing accounts to be moved from
+      "arn:aws:organizations::${data.aws_caller_identity.current.account_id}:ou/${aws_organizations_organization.default.id}/${aws_organizations_organizational_unit.platforms_and_architecture_modernisation_platform.id}",
+
+      // Any nested OUs under the parent OU
+      "arn:aws:organizations::${data.aws_caller_identity.current.account_id}:ou/${aws_organizations_organization.default.id}/*",
+
+      // Destination for moved account
+      "arn:aws:organizations::${data.aws_caller_identity.current.account_id}:root/${aws_organizations_organization.default.id}/${aws_organizations_organization.default.roots[0].id}"
+    ]
+  }
 }
 
 # Modernisation Platform end user permission sets are now managed in the modernisation-platform repository
