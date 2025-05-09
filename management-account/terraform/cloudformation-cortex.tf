044b4eb40ad8369f0292af2483c39cb59fee1b0b
David Sibley <david.sibley@digital.justice.gov.uk>
2025-05-09 12:42:30 +0100

    moved XDR root account stack across to management-account terraform
    removed lingering depends_on
    
    linted versions and added random provider to management account

---
+++ management-account/terraform/cloudformation-cortex.tf
@@ -0,0 +1,33 @@
+data "aws_s3_objects" "cortex_xdr_templates" {
+  bucket = module.cf_template_storage.bucket_name
+  prefix = "cortex-xdr"
+}
+
+data "aws_s3_object" "cortex_xdr_templates" {
+  for_each = toset(data.aws_s3_objects.cortex_xdr_templates.keys)
+  bucket   = module.cf_template_storage.bucket_name
+  key      = each.key
+}
+
+resource "random_uuid" "cortex_xdr_stack" {}
+
+resource "aws_ssm_parameter" "cortex_xdr_uuids" {
+  description = "Map of random UUID values used to secure external access for IAM role assumption"
+  name        = "cortex_xdr_uuids"
+  tags        = local.tags_organisation_management
+  type        = "SecureString"
+  value = jsonencode({
+    xdr_stack = random_uuid.cortex_xdr_stack.result
+  })
+}
+
+resource "aws_cloudformation_stack" "cortex_xdr_stack" {
+  capabilities = ["CAPABILITY_NAMED_IAM"]
+  name         = "CortexXDRCloudApp"
+  parameters = {
+    CortexXDRRoleName = "CortexXDRCloudApp",
+    ExternalID        = sensitive(random_uuid.cortex_xdr_stack.result)
+  }
+  tags          = local.tags_organisation_management
+  template_body = data.aws_s3_object.cortex_xdr_templates["cortex-xdr-root-account.template"].body
+}
