28b575f51ee683bc38b99961f1b80e81a821ea1b
Kudzai-moj <kudzai.mtoko@digital.justice.gov.uk>
2025-05-09 16:34:11 +0100

    removing code for mod platform to remove ou accounts

--- management-account/terraform/sso-admin-permission-sets.tf
+++ management-account/terraform/sso-admin-permission-sets.tf
@@ -424,26 +424,6 @@ data "aws_iam_policy_document" "modernisation_platform_engineer" {
     ]
     resources = ["arn:aws:dynamodb:eu-west-2:${coalesce(local.modernisation_platform_accounts.modernisation_platform_id...)}:table/modernisation-platform-terraform-state-lock"]
   }
-   statement {
-    sid    = "ModernisationPlatformAccountsMoveAccountsInOU"
-    effect = "Allow"
-    actions = [
-      "organizations:MoveAccount"
-    ]
-    resources = [
-      "arn:aws:organizations::${data.aws_caller_identity.current.account_id}:ou/${aws_organizations_organization.default.id}/${aws_organizations_organizational_unit.platforms_and_architecture_modernisation_platform.id}"
-    ]
-  }
-  statement {
-    sid    = "ModernisationPlatformAccountsCloseAccount"
-    effect = "Allow"
-    actions = [
-      "organizations:CloseAccount"
-    ]
-    resources = [
-      "arn:aws:organizations::${data.aws_caller_identity.current.account_id}:account/${aws_organizations_organization.default.id}/*"
-    ]
-  }
 }
 
 # Modernisation Platform end user permission sets are now managed in the modernisation-platform repository
