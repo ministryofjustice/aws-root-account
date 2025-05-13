8496cede4b21ac2894b7c1ad421939641b71bbe5
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2025-05-13 08:46:21 +0100

    add mdoule to create infrastructure for integration of cost data with CDDO account

---
+++ management-account/terraform/cddo-integration.tf
@@ -0,0 +1,8 @@
+# Infrastructure for integration with CDDO central account: Data export, S3, and bucket replication
+
+module "cddo_infra" {
+source = "[github.com/co-cddo/terraform-aws-focus?ref=v1.1.0](https://www.google.com/url?q=http%3A%2F%2Fgithub.com%2Fco-cddo%2Fterraform-aws-focus%3Fref%3Dv1.1.0&sa=D&ust=1743337080000000&usg=AOvVaw0JmgkcLkM7mKLFE3qmMv2g)"
+
+destination_account_id = "203341582084"
+destination_bucket_name = "uk-gov-gds-cost-inbound"
+}
