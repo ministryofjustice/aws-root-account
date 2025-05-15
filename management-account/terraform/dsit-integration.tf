83dd101ded265334f735eee062768d4abf3d2117
julialawrence <julia.lawrence@digital.justice.gov.uk>
2025-05-15 15:37:11 +0100

    Adding Auditing Permission Set

--- management-account/terraform/dsit-integration.tf
+++ management-account/terraform/dsit-integration.tf
@@ -1,8 +1,8 @@
 # Infrastructure to integrate cost reporting with DSIT central account: Data export, S3, and bucket replication
 
 module "dsit_cost_integration" {
-source = "github.com/co-cddo/terraform-aws-focus?ref=v1.1.0"
+  source = "github.com/co-cddo/terraform-aws-focus?ref=v1.1.0"
 
-destination_account_id = "203341582084"
-destination_bucket_name = "uk-gov-gds-cost-inbound"
+  destination_account_id  = "203341582084"
+  destination_bucket_name = "uk-gov-gds-cost-inbound"
 }
--- management-account/terraform/sso-admin-account-assignments.tf
+++ management-account/terraform/sso-admin-account-assignments.tf
@@ -411,6 +411,14 @@ locals {
       ]
     },
     {
+      github_team        = "laa-internal-analysis",
+      permission_set_arn = aws_ssoadmin_permission_set.security_audit.arn,
+      account_ids = [
+        aws_organizations_account.organisation_security.id,
+        aws_organizations_account.cloud_platform.id
+      ]
+    },
+    {
       github_team        = "modernisation-platform",
       permission_set_arn = aws_ssoadmin_permission_set.security_audit.arn,
       account_ids = [
