c63c8e1f03d477191db2be1c457e837e55437101
Stefan Hristov <stefan.hristov@justice.gov.uk>
2025-06-19 13:19:09 +0100

    Adding new Cost category Budgets
    Oops. Forgot to run terraform fmt
    
    Signed commit
    
    squashing all commits into one signed commit

---
+++ management-account/terraform/cost-categories-budgets.tf
@@ -0,0 +1,51 @@
+resource "aws_ce_cost_category" "budgets" {
+  name         = "Budgets"
+  rule_version = "CostCategoryExpression.v1"
+
+  rule {
+    value = "OPG"
+
+    rule {
+      dimension {
+        key           = "LINKED_ACCOUNT_NAME"
+        match_options = ["CONTAINS"]
+        values        = ["OPG", "opg", "LPA", "lpa"]
+      }
+    }
+  }
+
+  rule {
+    value = "Electronic Monitoring"
+
+    rule {
+      dimension {
+        key           = "LINKED_ACCOUNT_NAME"
+        match_options = ["STARTS_WITH"]
+        values        = ["Electronic Monitoring*", "electronic-monitring*"]
+      }
+    }
+  }
+
+  rule {
+    value = "Engineering"
+
+    rule {
+      not {
+        or {
+          dimension {
+            key           = "LINKED_ACCOUNT_NAME"
+            match_options = ["CONTAINS"]
+            values        = ["OPG", "opg", "LPA", "lpa"]
+          }
+        }
+        or {
+          dimension {
+            key           = "LINKED_ACCOUNT_NAME"
+            match_options = ["STARTS_WITH"]
+            values        = ["Electronic Monitoring*", "electronic-monitoring*"]
+          }
+        }
+      }
+    }
+  }
+}
