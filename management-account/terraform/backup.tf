2fb1ff9fd2ccb160cfec7d96f7d7785ae8de7c68
Jacob Woffenden <jacob@woffenden.io>
2025-06-20 16:16:30 +0100

    its false actually

--- management-account/terraform/backup.tf
+++ management-account/terraform/backup.tf
@@ -3,7 +3,7 @@ resource "aws_backup_global_settings" "eu_west_1" {
 
   global_settings = {
     "isCrossAccountBackupEnabled" = "true"
-    "isMpaEnabled"                = "true"
+    "isMpaEnabled"                = "false"
   }
 
   depends_on = [aws_organizations_organization.default]
