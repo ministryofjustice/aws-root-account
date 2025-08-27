64e9cbdf210a038d41367ec57aef4baa549dbc07
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2025-08-27 09:52:23 +0100

    Update organisation-security/terraform/account.tf
    Co-authored-by: ewastempel <52969218+ewastempel@users.noreply.github.com>

--- organisation-security/terraform/account.tf
+++ organisation-security/terraform/account.tf
@@ -1,7 +1,7 @@
 resource "aws_account_alternate_contact" "operations" {
   alternate_contact_type = "OPERATIONS"
   email_address          = "modernisation-platform@digital.justice.gov.uk"
-  name                   = "Modernisation Platform Team"
+  name                   = "Modernisation Platform"
   title                  = "Team"
   phone_number           = "+0000000000"
 }
