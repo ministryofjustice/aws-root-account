e03f5b3e2365b0d563ef9f47c5982ef929cc4e03
khatraf <khatra.farah@digital.justice.gov.uk>
2025-10-06 14:07:09 +0100

    Enable Inspector auto-enable for EC2, ECR, and Lambda for Security Hub compliance

--- organisation-security/terraform/inspector.tf
+++ organisation-security/terraform/inspector.tf
@@ -1,5 +1,5 @@
 ###########################
-# Inspector in EU regions #
+# Inspector in EU and US-East regions #
 ###########################
 
 # Auto enable per region
@@ -10,11 +10,35 @@ resource "aws_inspector2_organization_configuration" "eu_west_2" {
     lambda = true
   }
 }
-
 resource "aws_inspector2_organization_configuration" "eu_west_1" {
   auto_enable {
     ec2    = true
     ecr    = true
     lambda = true
+  }
+}
+resource "aws_inspector2_organization_configuration" "eu_west_3" {
+  auto_enable {
+    ec2    = true
+    ecr    = true
+    lambda = true
+  }
+}
+resource "aws_inspector2_organization_configuration" "eu_central_1" {
+  auto_enable {
+    ec2         = true
+    ecr         = true
+    lambda      = true
+    lambda_code = true
+
+  }
+}
+resource "aws_inspector2_organization_configuration" "us_east_1" {
+  auto_enable {
+    ec2         = true
+    ecr         = true
+    lambda      = true
+    lambda_code = true
+
   }
 }
