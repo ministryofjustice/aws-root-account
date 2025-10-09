f0cce7456b9d1ffe8f5338aeee7359120e07e2e1
khatraf <khatra.farah@digital.justice.gov.uk>
2025-10-09 16:23:22 +0100

    fix formatting

--- organisation-security/terraform/inspector.tf
+++ organisation-security/terraform/inspector.tf
@@ -5,28 +5,31 @@
 # Auto enable per region
 resource "aws_inspector2_organization_configuration" "eu_west_2" {
   auto_enable {
-    ec2    = true
-    ecr    = true
-    lambda = true
+    ec2         = true
+    ecr         = true
+    lambda      = true
     lambda_code = true
   }
 }
+
 resource "aws_inspector2_organization_configuration" "eu_west_1" {
   auto_enable {
-    ec2    = true
-    ecr    = true
-    lambda = true
+    ec2         = true
+    ecr         = true
+    lambda      = true
     lambda_code = true
   }
 }
+
 resource "aws_inspector2_organization_configuration" "eu_west_3" {
   auto_enable {
-    ec2    = true
-    ecr    = true
-    lambda = true
+    ec2         = true
+    ecr         = true
+    lambda      = true
     lambda_code = false
   }
 }
+
 resource "aws_inspector2_organization_configuration" "eu_central_1" {
   auto_enable {
     ec2         = true
@@ -35,6 +38,7 @@ resource "aws_inspector2_organization_configuration" "eu_central_1" {
     lambda_code = true
   }
 }
+
 resource "aws_inspector2_organization_configuration" "us_east_1" {
   auto_enable {
     ec2         = true
