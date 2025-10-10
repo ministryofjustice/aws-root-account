06b8df61f04e039d22b353eefb42a326bed435fd
khatraf <khatra.farah@digital.justice.gov.uk>
2025-10-10 12:27:18 +0100

    Add aws_inspector2_enabler for new regions.

--- organisation-security/terraform/inspector.tf
+++ organisation-security/terraform/inspector.tf
@@ -2,6 +2,34 @@
 # Inspector in EU and US-East regions #
 ###########################
 
+# Enable Inspector in each region where it's not currently active
+resource "aws_inspector2_enabler" "eu_west_3" {
+  provider = aws.eu-west-3
+  account_ids = [
+    data.aws_caller_identity.current.account_id,
+    local.root_account_id
+  ]
+  resource_types = ["EC2", "ECR", "LAMBDA"]
+}
+
+resource "aws_inspector2_enabler" "eu_central_1" {
+  provider = aws.eu-central-1
+  account_ids = [
+    data.aws_caller_identity.current.account_id,
+    local.root_account_id
+  ]
+  resource_types = ["ECR", "EC2", "LAMBDA", "LAMBDA_CODE"]
+}
+
+resource "aws_inspector2_enabler" "us_east_1" {
+  provider = aws.us-east-1
+  account_ids = [
+    data.aws_caller_identity.current.account_id,
+    local.root_account_id
+  ]
+  resource_types = ["ECR", "EC2", "LAMBDA", "LAMBDA_CODE"]
+}
+
 # Auto enable per region
 resource "aws_inspector2_organization_configuration" "eu_west_2" {
   auto_enable {
@@ -24,10 +52,9 @@ resource "aws_inspector2_organization_configuration" "eu_west_1" {
 resource "aws_inspector2_organization_configuration" "eu_west_3" {
   provider = aws.eu-west-3
   auto_enable {
-    ec2         = true
-    ecr         = true
-    lambda      = true
-    lambda_code = false
+    ec2    = true
+    ecr    = true
+    lambda = true
   }
 }
 
