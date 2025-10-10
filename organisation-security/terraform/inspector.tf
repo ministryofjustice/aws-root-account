5a0a249680289b83a96c096b1f4647c037e62002
khatraf <khatra.farah@digital.justice.gov.uk>
2025-10-10 15:20:55 +0100

    separate resources required for admin and member accounts

--- organisation-security/terraform/inspector.tf
+++ organisation-security/terraform/inspector.tf
@@ -5,12 +5,31 @@
 # Data source to get all organization accounts
 data "aws_organizations_organization" "current" {}
 
-# Enable Inspector2 for all organization member accounts including delegated admin in new regions
+# Enable Inspector2 for all existing organization member accounts in new regions
+
+resource "aws_inspector2_enabler" "eu_west_3" {
+  provider       = aws.eu-west-3
+  account_ids    = [data.aws_caller_identity.current.account_id]
+  resource_types = ["EC2", "ECR", "LAMBDA"]
+}
+
+resource "aws_inspector2_enabler" "eu_central_1" {
+  provider       = aws.eu-central-1
+  account_ids    = [data.aws_caller_identity.current.account_id]
+  resource_types = ["ECR", "EC2", "LAMBDA", "LAMBDA_CODE"]
+}
+
+resource "aws_inspector2_enabler" "us_east_1" {
+  provider       = aws.us-east-1
+  account_ids    = [data.aws_caller_identity.current.account_id]
+  resource_types = ["ECR", "EC2", "LAMBDA", "LAMBDA_CODE"]
+}
+
 resource "aws_inspector2_enabler" "all_member_accounts_eu_west_3" {
   provider = aws.eu-west-3
   account_ids = [
     for account in data.aws_organizations_organization.current.accounts :
-    account.id if account.id != local.root_account_id
+    account.id if account.id != local.root_account_id && account.id != data.aws_caller_identity.current.account_id
   ]
   resource_types = ["EC2", "ECR", "LAMBDA"]
 }
@@ -19,7 +38,7 @@ resource "aws_inspector2_enabler" "all_member_accounts_eu_central_1" {
   provider = aws.eu-central-1
   account_ids = [
     for account in data.aws_organizations_organization.current.accounts :
-    account.id if account.id != local.root_account_id
+    account.id if account.id != local.root_account_id && account.id != data.aws_caller_identity.current.account_id
   ]
   resource_types = ["EC2", "ECR", "LAMBDA", "LAMBDA_CODE"]
 }
@@ -28,7 +47,7 @@ resource "aws_inspector2_enabler" "all_member_accounts_us_east_1" {
   provider = aws.us-east-1
   account_ids = [
     for account in data.aws_organizations_organization.current.accounts :
-    account.id if account.id != local.root_account_id
+    account.id if account.id != local.root_account_id && account.id != data.aws_caller_identity.current.account_id
   ]
   resource_types = ["EC2", "ECR", "LAMBDA", "LAMBDA_CODE"]
 }
