ba39a38484807a97fd714e303645ad6a94175b32
khatraf <khatra.farah@digital.justice.gov.uk>
2025-10-16 11:01:30 +0100

    replaced count with for_each for better change handling

--- organisation-security/terraform/inspector.tf
+++ organisation-security/terraform/inspector.tf
@@ -7,9 +7,9 @@ data "aws_organizations_organization" "current" {}
 
 # Local value to chunk accounts into batches of 100 (AWS API limit)
 locals {
- all_member_accounts = [
+  all_member_accounts = [
     for account in data.aws_organizations_organization.current.accounts :
-    account.id if account.id != local.root_account_id && account.id != data.aws_caller_identity.current.account_id  &&
+    account.id if account.id != local.root_account_id && account.id != data.aws_caller_identity.current.account_id &&
     account.status == "ACTIVE"
   ]
 
@@ -40,24 +40,24 @@ resource "aws_inspector2_enabler" "us_east_1" {
   resource_types = ["ECR", "EC2", "LAMBDA", "LAMBDA_CODE"]
 }
 resource "aws_inspector2_enabler" "all_member_accounts_eu_west_3" {
+  for_each       = { for idx, chunk in local.account_chunks : idx => chunk }
   provider       = aws.eu-west-3
-  account_ids    = local.all_member_accounts
+  account_ids    = each.value
   resource_types = ["EC2", "ECR", "LAMBDA"]
-  depends_on     = [aws_inspector2_organization_configuration.eu_west_3]
 }
 
 resource "aws_inspector2_enabler" "all_member_accounts_eu_central_1" {
+  for_each       = { for idx, chunk in local.account_chunks : idx => chunk }
   provider       = aws.eu-central-1
-  account_ids    = local.all_member_accounts
+  account_ids    = each.value
   resource_types = ["EC2", "ECR", "LAMBDA", "LAMBDA_CODE"]
-  depends_on     = [aws_inspector2_organization_configuration.eu_central_1]
 }
 
 resource "aws_inspector2_enabler" "all_member_accounts_us_east_1" {
+  for_each       = { for idx, chunk in local.account_chunks : idx => chunk }
   provider       = aws.us-east-1
-  account_ids    = local.all_member_accounts
+  account_ids    = each.value
   resource_types = ["EC2", "ECR", "LAMBDA", "LAMBDA_CODE"]
-  depends_on     = [aws_inspector2_organization_configuration.us_east_1]
 }
 
 # Auto enable per region
@@ -110,19 +110,19 @@ resource "aws_inspector2_organization_configuration" "us_east_1" {
 
 # Member associations for explicit association with delegated admin account
 resource "aws_inspector2_member_association" "us_east_1" {
-  count      = length(local.all_member_accounts)
+  for_each   = toset(local.all_member_accounts)
   provider   = aws.us-east-1
-  account_id = local.all_member_accounts[count.index]
+  account_id = each.value
 }
 
 resource "aws_inspector2_member_association" "eu_central_1" {
-  count      = length(local.all_member_accounts)
+  for_each   = toset(local.all_member_accounts)
   provider   = aws.eu-central-1
-  account_id = local.all_member_accounts[count.index]
+  account_id = each.value
 }
 
 resource "aws_inspector2_member_association" "eu_west_3" {
-  count      = length(local.all_member_accounts)
+  for_each   = toset(local.all_member_accounts)
   provider   = aws.eu-west-3
-  account_id = local.all_member_accounts[count.index]
+  account_id = each.value
 }
