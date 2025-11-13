15e3ec1f0e4b066fc029628b4f5099dfbb1459a0
Aaron Robinson <aaron.robinson1@justice.gov.uk>
2025-11-13 12:17:08 +0000

    add version comment

--- management-account/terraform/sso-scim.tf
+++ management-account/terraform/sso-scim.tf
@@ -1,6 +1,6 @@
 module "scim" {
   # tflint-ignore: terraform_module_pinned_source
-  source                = "github.com/ministryofjustice/moj-terraform-scim-github?ref=52a981e832f47fd75f023c647913ca1a8d9ac907"
+  source                = "github.com/ministryofjustice/moj-terraform-scim-github?ref=52a981e832f47fd75f023c647913ca1a8d9ac907" # v1.0.0
   github_organisation   = local.sso.github_organisation
   github_token          = sensitive(local.sso.aws_saml.github_token)
   sso_aws_region        = local.sso.region
@@ -11,7 +11,7 @@ module "scim" {
 
 module "entraid_scim" {
   # tflint-ignore: terraform_module_pinned_source
-  source              = "github.com/ministryofjustice/moj-terraform-scim-entra-id?ref=61ed386f4d5cd07d3411c73e1bd2f790224c4964"
+  source              = "github.com/ministryofjustice/moj-terraform-scim-entra-id?ref=61ed386f4d5cd07d3411c73e1bd2f790224c4964" # v1.0.0
   azure_tenant_id     = sensitive(local.azure.tenant_id)
   azure_client_id     = sensitive(local.azure.client_id)
   azure_client_secret = sensitive(local.azure.client_secret)
