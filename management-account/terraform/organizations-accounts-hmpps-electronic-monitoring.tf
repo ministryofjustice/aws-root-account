1656ccd5a9bcf63f6ec5070420a7b9d0097a8a9c
David Elliott <david.elliott@digital.justice.gov.uk>
2025-12-10 12:10:19 +0000

    Delete EM accounts
    As per request here - https://mojdt.slack.com/archives/C06P4KA0V0A/p1765288983337289
    Deleting EM accounts.

--- management-account/terraform/organizations-accounts-hmpps-electronic-monitoring.tf
+++ management-account/terraform/organizations-accounts-hmpps-electronic-monitoring.tf
@@ -23,48 +23,6 @@ resource "aws_organizations_account" "electronic_monitoring_identity_and_access_
   }
 }
 
-# Electronic Monitoring Infrastructure Dev
-resource "aws_organizations_account" "electronic_monitoring_infrastructure_dev" {
-  name                       = "Electronic Monitoring Infrastructure Dev"
-  email                      = replace(local.aws_account_email_addresses_template, "{email}", "hmpps-em-infra-dev")
-  iam_user_access_to_billing = "ALLOW"
-  parent_id                  = aws_organizations_organizational_unit.hmpps_electronic_monitoring.id
-
-  tags = merge(local.tags_electronic_monitoring, {
-
-  })
-
-  lifecycle {
-    ignore_changes = [
-      email,
-      iam_user_access_to_billing,
-      name,
-      role_name,
-    ]
-  }
-}
-
-# Electronic Monitoring Protective Monitoring
-resource "aws_organizations_account" "electronic_monitoring_protective_monitoring" {
-  name                       = "Electronic Monitoring Protective Monitoring"
-  email                      = replace(local.aws_account_email_addresses_template, "{email}", "hmpps-em-promon")
-  iam_user_access_to_billing = "ALLOW"
-  parent_id                  = aws_organizations_organizational_unit.hmpps_electronic_monitoring.id
-
-  tags = merge(local.tags_electronic_monitoring, {
-
-  })
-
-  lifecycle {
-    ignore_changes = [
-      email,
-      iam_user_access_to_billing,
-      name,
-      role_name,
-    ]
-  }
-}
-
 # Electronic Monitoring Shared Logging
 resource "aws_organizations_account" "electronic_monitoring_shared_logging" {
   name                       = "Electronic Monitoring Shared Logging"
