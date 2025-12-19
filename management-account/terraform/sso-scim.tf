6623bb62a64c10337cbe4901c5eae88661e10431
Aaron Robinson <aaron.robinson1@justice.gov.uk>
2025-12-19 12:31:55 +0000

    fix terraform fmt error

--- management-account/terraform/sso-scim.tf
+++ management-account/terraform/sso-scim.tf
@@ -32,7 +32,7 @@ module "scim_slack_notifications" {
 
   application_name = "scim-monitoring"
   slack_channel_id = "C02PFCG8M1R"
-  sns_topic_arns   = [
+  sns_topic_arns = [
     module.entraid_scim.sns_topic_arn,
     module.scim.sns_topic_arn
   ]
