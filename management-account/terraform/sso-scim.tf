4946fb7b0e970326c02826f90ac0f15a0552aad8
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2026-02-10 13:50:18 +0000

    ⬆️ Update GitHub SCIM to Ignore Business Units Team (#1423)

--- management-account/terraform/sso-scim.tf
+++ management-account/terraform/sso-scim.tf
@@ -1,6 +1,6 @@
 module "scim" {
   # tflint-ignore: terraform_module_pinned_source
-  source                     = "github.com/ministryofjustice/moj-terraform-scim-github?ref=9c77b1cde00919cda5cedb4d5d93cb880453237e" # v3.2.1
+  source                     = "github.com/ministryofjustice/moj-terraform-scim-github?ref=1badc67aa438f0d11b62e080fa39476fe7178180" # v3.3.0
   github_organisation        = local.sso.github_organisation
   github_app_id              = sensitive(local.sso.aws_saml.github_app_id)
   github_app_installation_id = sensitive(local.sso.aws_saml.github_app_installation_id)
