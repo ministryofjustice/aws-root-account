e36548c20a429b3b5415ebf267b142ad386482ca
connormaglynn <connor.glynn@justice.gov.uk>
2026-03-06 16:07:10 +0000

    Update

--- management-account/terraform/cost-categories-modernisation-platform.tf
+++ management-account/terraform/cost-categories-modernisation-platform.tf
@@ -1,6 +1,7 @@
 resource "aws_ce_cost_category" "modernisation_platform" {
   name            = "Modernisation Platform"
   rule_version    = "CostCategoryExpression.v1"
+  effective_start = "2026-02-01T00:00:00Z"
 
   rule {
     type  = "REGULAR"
