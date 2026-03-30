d00fcc88c9bb0a0a1e16c5a957243fb19af0acd3
khatraf <khatra.farah@digital.justice.gov.uk>
2026-03-30 16:24:23 +0100

    adds remove blocks to DSIT resources

--- management-account/terraform/dsit-integration.tf
+++ management-account/terraform/dsit-integration.tf
@@ -12,3 +12,108 @@ import {
   to = module.dsit_cost_integration.aws_costoptimizationhub_enrollment_status.this[0]
   id = data.aws_caller_identity.current.account_id
 }
+
+removed {
+  from = module.dsit_cost_integration.aws_bcmdataexports_export.carbon
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.dsit_cost_integration.aws_bcmdataexports_export.focus
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.dsit_cost_integration.aws_bcmdataexports_export.recommendations
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.dsit_cost_integration.aws_costoptimizationhub_enrollment_status.this
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.dsit_cost_integration.aws_iam_role.this
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.dsit_cost_integration.aws_iam_role_policy.replicator
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.dsit_cost_integration.aws_iam_service_linked_role.bcm_data_exports
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.dsit_cost_integration.aws_s3_bucket.this
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.dsit_cost_integration.aws_s3_bucket_lifecycle_configuration.this
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.dsit_cost_integration.aws_s3_bucket_policy.this
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.dsit_cost_integration.aws_s3_bucket_replication_configuration.this
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.dsit_cost_integration.aws_s3_bucket_versioning.this
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.dsit_cost_integration.time_sleep.service_linked_role
+
+  lifecycle {
+    destroy = false
+  }
+}
+
