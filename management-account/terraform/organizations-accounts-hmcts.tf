2ea3cde52b2d8cb6f976b25086d82229bec24f82
connormaglynn <connor.glynn@justice.gov.uk>
2026-04-01 17:24:52 +0100

    🔥 Remove TP-HMCTS AWS Account

--- management-account/terraform/organizations-accounts-hmcts.tf
+++ management-account/terraform/organizations-accounts-hmcts.tf
@@ -44,22 +44,3 @@ resource "aws_organizations_account" "ministry_of_justice_courtfinder_prod" {
   }
 }
 
-# Contains IAC Fees - awaiting migration sign-off before closure
-resource "aws_organizations_account" "tp_hmcts" {
-  name                       = "TP-HMCTS"
-  email                      = replace(local.aws_account_email_addresses_template, "{email}", "TP-HMCTS")
-  iam_user_access_to_billing = "ALLOW"
-  parent_id                  = aws_organizations_organizational_unit.hmcts.id
-  close_on_deletion          = true
-
-  tags = local.tags_hmcts
-
-  lifecycle {
-    ignore_changes = [
-      email,
-      iam_user_access_to_billing,
-      name,
-      role_name,
-    ]
-  }
-}
