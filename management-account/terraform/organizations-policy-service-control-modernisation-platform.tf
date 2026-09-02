a635842387d7866b257ce8250e854050ec355968
Kudzai Mtoko <kudzai.mtoko@digital.justice.gov.uk>
2026-09-02 11:13:53 +0100

    Narrow Config and GuardDuty SCP delete protections

--- management-account/terraform/organizations-policy-service-control-modernisation-platform.tf
+++ management-account/terraform/organizations-policy-service-control-modernisation-platform.tf
@@ -281,10 +281,10 @@ data "aws_iam_policy_document" "mp_protect_secure_baselines" {
     sid    = "DenyConfigRecorderAndGuardDutyDetectorChanges"
     effect = "Deny"
     actions = [
-      "config:Delete*",
+      "config:DeleteConfigurationRecorder",
       "config:PutConfigurationRecorder",
       "config:StopConfigurationRecorder",
-      "guardduty:Delete*",
+      "guardduty:DeleteDetector",
       "guardduty:UpdateDetector"
     ]
     resources = ["*"]
@@ -295,7 +295,6 @@ data "aws_iam_policy_document" "mp_protect_secure_baselines" {
       values = [
         "arn:aws:iam::*:role/ModernisationPlatformAccess",
         "arn:aws:iam::*:role/github-actions",
-        "arn:aws:iam::*:role/MemberInfrastructureAccess",
         "arn:aws:iam::*:role/aws-reserved/sso.amazonaws.com/*/AWSReservedSSO_AdministratorAccess*"
       ]
     }
