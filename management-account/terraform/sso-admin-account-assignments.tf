a5c9e51cc1f15783b2c1dd9d871bcc21c80deaa1
julialawrence <98329494+julialawrence@users.noreply.github.com>
2026-09-16 09:33:23 +0100

    Update sso-admin-account-assignments.tf
    formatting

--- management-account/terraform/sso-admin-account-assignments.tf
+++ management-account/terraform/sso-admin-account-assignments.tf
@@ -102,7 +102,7 @@ locals {
         aws_organizations_account.hmpps_cr_unpaid_work_production.id,
       ]
     },
-        {
+    {
       github_team        = "hmpps-jenkins-admin",
       permission_set_arn = aws_ssoadmin_permission_set.administrator_access.arn,
       account_ids = [
